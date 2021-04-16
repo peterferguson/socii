50dc0bd6a25d8a568fabedd7301063edb505a5b7
Peter Ferguson <peterferguson95@gmail.com>
2021-04-16 10:35:55 +0100

    added popular logos to scraper

--- temp/logo_scraper.js
+++ temp/logo_scraper.js
@@ -1,8 +1,8 @@
 // Load T212 ISIN mapping
-const tickerMapping = require("/Users/peter/Projects/socii/t212_tickers.json");
+const tickerMapping = require("/Users/peter/Projects/socii/temp/t212_tickers.json");
 const bent = require("bent");
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("/Users/peter/Projects/socii/serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -19,11 +19,8 @@ async function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const loadLogos = async () => {
-  var count = 0;
-  for await (const ticker of tickerMapping) {
-    // Makes reference to the storage bucket location
-    fileUrl = `logos/${ticker.isin}.png`;
+const uploadLogo = (ticker) => {
+  fileUrl = `logos/${ticker.isin}.png`;
 
     const bucket = admin.storage().bucket();
     const file = bucket.file(fileUrl);
@@ -44,10 +41,31 @@ const loadLogos = async () => {
         });
       }
     });
+}
+
+const loadLogos = async () => {
+  var count = 0;
+  for await (const ticker of tickerMapping) {
+    // Makes reference to the storage bucket location
+    uploadLogo(ticker)
     await sleep(500);
     if (count > 100) break;
     count += 1;
   }
 };
 
-loadLogos();
+// loadLogos();
+
+const popularTickersLogos = async () => {
+  popularTickers=["US98980L1017" ,"LU1778762911" ,"US01609W1027" ,"US0231351067" ,"US0378331005" ,"US36467W1099" ,"US64110L1061" ,"US67066G1040" ,"US88160R1014" ,"US88339J1051" ,"US90353T1007"]
+  uploaded = [];
+  for await ( const ticker of tickerMapping) {
+    if (popularTickers.includes(ticker.isin) && !uploaded.includes(ticker.isin)) {
+      uploadLogo(ticker)
+      uploaded.push(ticker.isin)
+    }
+  }
+
+}
+
+popularTickersLogos()
