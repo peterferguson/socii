448831d5363e1b73ea0ffa9881eac1099ee2285b
Peter Ferguson <peterferguson95@gmail.com>
2021-04-29 16:31:54 +0100

    updated dropdown using popover

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -4,10 +4,11 @@ import SearchIcon from "@icons/search.svg";
 import CogIcon from "@icons/cog.svg";
 import PieIcon from "@icons/pie.svg";
 import AtIcon from "@icons/at.svg";
+import LogoutIcon from "@icons/logout.svg";
 import GroupIcon from "@icons/group.svg";
 import RightChevronIcon from "@icons/rightChevron.svg";
 import MenuIcon from "@icons/menu.svg";
-import { Menu, Transition } from "@headlessui/react";
+import { Menu, Transition, Popover } from "@headlessui/react";
 
 import Link from "next/link";
 import { useContext } from "react";
@@ -23,8 +24,7 @@ export default function Navigation(props) {
       <SearchBar {...props} />
       {username && (
         <>
-          {/* <DropdownMenu /> */}
-          <MyDropdown />
+          <MyPopover />
           {/* <Link href={`/${username}`} className="icon-button">
               {user?.photoUrl ? <img src={user?.photoUrl}></img> : <a>"🙋‍♂️"</a>}
             </Link> */}
@@ -91,120 +91,105 @@ function SearchBar({ setShowSearchCard }) {
   );
 }
 
-function MyDropdown() {
+const dropdownItems = [
+  {
+    name: "Invites",
+    description: "Invite yours friends to the alpha",
+    href: "##",
+    icon: AtIcon,
+  },
+  {
+    name: "Porfolio",
+    description: "Keep track of your growth",
+    href: "##",
+    icon: PieIcon,
+  },
+  {
+    name: "Groups",
+    description: "View all of your Groups",
+    href: "##",
+    icon: GroupIcon,
+    rightIcon: RightChevronIcon,
+  },
+  {
+    name: "Settings",
+    description: "Adjust your settings",
+    href: "##",
+    icon: CogIcon,
+    rightIcon: RightChevronIcon,
+  },
+];
+
+const grayedDropdownItems = [
+  {
+    name: "Sign Out",
+    description: "",
+    href: "",
+    icon: LogoutIcon,
+    onClick: () => signOut(router, userFirstName(user)),
+  },
+];
+
+function MyPopover() {
   return (
-    <Menu as="div" className="relative inline-block text-left p-4">
+    <Popover className="relative inline-block text-left p-4">
       {({ open }) => (
         <>
-          <Menu.Button className="items-center justify-center w-full h-full focus:outline-none">
+          <Popover.Button className="items-center justify-center w-full h-full focus:outline-none">
             <MenuIcon className=" w-6 h-6 text-gray-300" />
-          </Menu.Button>
+          </Popover.Button>
           <Transition
             show={open}
             as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
           >
-            <Menu.Items
+            <Popover.Panel
               static
-              className="absolute right-0 w-56 mt-2 mr-2 origin-top-right 
-                       bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 \
-                         ring-black ring-opacity-5 focus:outline-none"
+              className="absolute z-10 w-max px-6 mt-20 -top-0.5 right-2 sm:px-0 lg:max-w-3xl"
             >
-              <div className="px-1 py-1 ">
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      onClick={() => signOut(router, userFirstName(user))}
-                      className={`${
-                        active ? "text-brand-light" : "text-gray-900"
-                      } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                    >
-                      <PieIcon
-                        className={"w-5 h-5 mr-2 text-brand"}
-                        aria-hidden="true"
-                      />
-                      Portfolios
-                    </button>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      onClick={() => signOut(router, userFirstName(user))}
-                      className={`${
-                        active ? "text-brand-light" : "text-gray-900"
-                      } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                    >
-                      <AtIcon
-                        className={"w-5 h-5 mr-2 text-brand"}
-                        aria-hidden="true"
-                      />
-                      Invites
-                    </button>
-                  )}
-                </Menu.Item>
+              <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="relative grid gap-8 bg-white p-7">
+                  {dropdownItems.map((item) => (
+                    <DropdownItem item={item} />
+                  ))}
+                </div>
+                <div className="p-4 bg-gray-50">
+                  {grayedDropdownItems.map((item) => (
+                    <DropdownItem item={item} />
+                  ))}
+                </div>
               </div>
-              <div className="px-1 py-1">
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      className={`${
-                        active ? "text-brand-light" : "text-gray-900"
-                      } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                    >
-                      <GroupIcon
-                        className={"flex-none w-5 h-5 mr-2 text-brand"}
-                        aria-hidden="true"
-                      />
-                      <span className="flex-grow text-left">Groups</span>
-                      <RightChevronIcon className="flex-none h-4 w-4" />
-                    </button>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    
-                    <button
-                      className={`${
-                        active ? "text-brand-light" : "text-gray-900"
-                      } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                    >
-                      <CogIcon
-                        className={"flex-none w-5 h-5 mr-2 text-brand"}
-                        aria-hidden="true"
-                      />
-                      <span className="flex-grow text-left">Settings</span>
-                      <RightChevronIcon className="flex-none h-4 w-4" />
-                    </button>
-                  )}
-                </Menu.Item>
-              </div>
-              <div className="px-1 py-1">
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      onClick={() => signOut(router, userFirstName(user))}
-                      className={`${
-                        active ? "text-brand-light" : "text-gray-900"
-                      } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                    >
-                      <span className={"w-5 h-5 mr-2"} aria-hidden="true">
-                        👋
-                      </span>
-                      Sign Out
-                    </button>
-                  )}
-                </Menu.Item>
-              </div>
-            </Menu.Items>
+            </Popover.Panel>
           </Transition>
         </>
       )}
-    </Menu>
+    </Popover>
+  );
+}
+
+function DropdownItem({ item }) {
+  return (
+    <a
+      key={item.name}
+      href={item.href}
+      onClick={item.onClick}
+      className="flex items-center p-2 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-gray-50 focus:outline-none"
+    >
+      <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 text-white sm:h-12 sm:w-12">
+        <item.icon className={"w-6 h-6 mr-2 text-brand"} aria-hidden="true" />
+      </div>
+      <div className="ml-4 flex-grow">
+        <p className="text-sm font-medium text-gray-900">{item.name}</p>
+        <p className="text-sm text-gray-500">{item.description}</p>
+      </div>
+      {item.rightIcon && (
+        <item.rightIcon className="flex-none h-6 w-6 text-brand-light" aria-hidden="true" />
+      )}
+    </a>
   );
 }
