599b6f3f28a2f6e0b5c19317f40e2fed8b4cd4ad
Peter Ferguson <peterferguson95@gmail.com>
2021-05-01 09:58:24 +0100

    🐛 window sizing for trading view chart

--- components/TradingViewChart.tsx
+++ components/TradingViewChart.tsx
@@ -1,7 +1,9 @@
 import TradingViewWidget, { Themes } from 'react-tradingview-widget';
+import { useWindowSize } from "@lib/hooks";
 
 export default function TradingViewChart(props){
     const tradingViewTicker = `NASDAQ:${props.tickerSymbol}`;
+    const [width, height] = useWindowSize();
     return (
         <TradingViewWidget
         symbol={tradingViewTicker}
@@ -9,7 +11,9 @@ export default function TradingViewChart(props){
         theme={Themes.DARK}
         locale="en"
         allow_symbol_change={false}
-        autosize={true}
+        height={height*0.7}
+        width={width*0.9}
+        // autosize={true}
       />
     )
 }
