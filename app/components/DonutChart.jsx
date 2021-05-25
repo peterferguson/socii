1ac4a4bf55a6ad5042bef780a1ab9b0703571f9d
Peter Ferguson <peterferguson95@gmail.com>
2021-05-25 14:11:51 +0100

    💄 updated pie UI

--- app/components/DonutChart.jsx
+++ app/components/DonutChart.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import { RadialChart, Hint } from 'react-vis'
-import { useWindowSize } from '@lib/hooks'
-import 'react-vis/dist/style.css'
-import { pnlTextColor } from '@utils/helper'
+import React, { useState } from "react"
+import { RadialChart, Hint } from "react-vis"
+import { useWindowSize } from "@lib/hooks"
+import "react-vis/dist/style.css"
+import { pnlTextColor } from "@utils/helper"
 
 export default function DonutChart({
   data,
@@ -15,11 +15,11 @@ export default function DonutChart({
 }) {
   const [value, setValue] = useState(false)
 
-  let width = 350
-  let height = 300
+  let [width, height] = useWindowSize()
 
-  if (scaleToWindow) {
-    ;[width, height] = useWindowSize()
+  if (!scaleToWindow) {
+    width = 350
+    height = 300
   }
 
   const chartProps = {
@@ -33,20 +33,23 @@ export default function DonutChart({
     onValueMouseOver: (v) => setValue(v),
     onSeriesMouseOut: () => setValue(false),
     padAngle: 0.02,
-    colorType: skeleton ? 'literal' : 'linear',
+    colorType: skeleton ? "literal" : "linear",
   }
 
   // TODO: Add skeleton ternary for the portfolio header
   return (
     <RadialChart {...chartProps}>
-      <div className="w-full mx-auto text-xl text-center text-gray-600 font-poppins -mt-46 z-1">
+      <div className="w-full mx-auto text-xl text-center text-gray-600 font-poppins -mt-52 z-1">
         <div className="uppercase text-tiny leading-4 ">portfolio</div>
         {text.main}
         <div
           className={`font-poppins text-lg mx-auto ${pnlTextColor(
             parseFloat(text.sub)
-          )}`}
+            )}`}
         >
+          <div className="w-5/12 h-1 my-3 border-gray-200 ml-[6.5rem] border-b-[0.5px]" />
+            <div className="uppercase text-tiny leading-4 ">gain</div>
+           
           {text.sub}
         </div>
       </div>
