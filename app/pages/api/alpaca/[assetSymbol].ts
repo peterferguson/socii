1db9db80020a06d6d7c8b3a8ecb7e4cb4cc54302
Peter Ferguson <peterferguson95@gmail.com>
2021-07-08 23:25:17 +0100

    query any symbol on broker api

---
+++ app/pages/api/alpaca/[assetSymbol].ts
@@ -0,0 +1,15 @@
+import { AssetsApi } from "@lib/alpaca/api"
+import { NextApiRequest, NextApiResponse } from "next"
+import { cors } from "@utils/middleware"
+
+const assetClient = new AssetsApi(process.env.ALPACA_KEY, process.env.ALPACA_SECRET)
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await cors(req, res)
+
+  if (req.method !== "GET") return res.status(405).end()
+
+  const { assetSymbol } = req.query as { assetSymbol: string }
+
+  res.end(JSON.stringify((await assetClient.assetsSymbolGet(assetSymbol)).body))
+}
