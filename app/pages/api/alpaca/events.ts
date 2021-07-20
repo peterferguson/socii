d06daedd7bd8222f79fa2a1e7fb1f92933d7a217
Peter Ferguson <peterferguson95@gmail.com>
2021-07-20 10:46:43 +0100

    added poc events streaming function

--- app/pages/api/alpaca/events.ts
+++ app/pages/api/alpaca/events.ts
@@ -1,26 +1,115 @@
 // ! Events in the [documentation](https://alpaca.markets/docs/broker/api-references/events/)
 // ! do not match up to those in the openapi.yaml!
-// // handler function for alpaca/apis/EventAPI.js
-// import { config, EventsApi } from "@alpaca/index"
-// import { withAuth, withCORS } from "@utils/middleware"
-// import { NextApiRequest, NextApiResponse } from "next"
+import { EventsApiRequestFactory } from "@alpaca/apis/EventsApi"
+import { config } from "@alpaca/index"
+import { withAuth, withCORS } from "@utils/middleware"
+import https, { IncomingMessage } from "http"
+import { NextApiRequest, NextApiResponse } from "next"
 
-// const eventsClient = new EventsApi(config)
+const eventsRequestFactory = new EventsApiRequestFactory(config)
 
-// export async function handleOrders(req: NextApiRequest, res: NextApiResponse) {
-//   const { body, method } = req
+// ! The naive approach doesn't work since the sse is not encoded in the OAS3 spec
+// const events = new EventsApi(config)
+// events.eventsJournalsStatusGet(since).then((r) => console.log(r))
 
-//   switch (method) {
-//     case "GET":
-//       /* Get subscription to account status events */
-//       const accountSub = eventsClient.eventsAccountsStatusGet()
+// ! This wil expose all the events from alpaca to the client!
+// ! We need to filter out the events that don't apply to that user!
+// ! Or implement some cheap server to store these events in firestore!
 
-//       break
-//     default:
-//       res.setHeader("Allow", ["PUT", "POST", "DELETE"])
-//       res.status(405).end(`Method ${method} Not Allowed`)
-//   }
-// }
+export default withCORS(handleEvents)
 
-// export default withAuth(withCORS(handleOrders))
-export {}
+export async function handleEvents(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    body: { type, since, until, sinceId, untilId },
+    method,
+  } = req
+
+  if (method !== "POST") {
+    res.setHeader("Allow", ["POST"])
+    res.status(405).end(`Method ${method} Not Allowed`)
+  }
+
+  const queryParams = { since, until, sinceId, untilId }
+
+  const query = Object.keys(queryParams)
+    .filter((key) => queryParams[key] !== undefined)
+    .map((key) => `${key}=${queryParams[key]}`)
+    .toString()
+
+  const baseServer: BaseServer = {
+    url: "",
+    variableConfiguration: undefined,
+    ...config.baseServer,
+  }
+
+  const alpacaEventEndpoints = {
+    accounts: "accounts/status",
+    journals: "journels/status",
+    trades: "trades",
+    transfers: "transfers/status",
+    "non-trading-activity": "nta",
+  }
+  console.log(`${baseServer.url}/${alpacaEventEndpoints[type]}?${query}`)
+
+  if (type in alpacaEventEndpoints) {
+    try {
+      await sseGetRequest(
+        `${baseServer.url}/${alpacaEventEndpoints[type]}?${query}`,
+        httpResponseCallback(res, type)
+      )
+    } catch (e) {
+      console.log(e)
+    }
+  } else {
+    res.status(422).end(`Please provide one of the following types:
+       - "accounts"
+       - "journals"
+       - "trades"
+       - "transfers"
+       - "non-trading-activity"
+      as \`type\` in the request body.`)
+  }
+}
+
+const sseGetRequest = async (
+  url: string,
+  responseCallback: (res: IncomingMessage) => void
+) => {
+  const headers = {
+    Authorization: `Basic ${Buffer.from(
+      process.env.ALPACA_KEY + ":" + process.env.ALPACA_SECRET
+    ).toString("base64")}`,
+    "content-type": "text/event-stream",
+  }
+  https.get(url, { headers }, responseCallback).on("error", (err) => {
+    console.log("HTTP Request Error: ", err.message)
+  })
+}
+
+interface BaseServer {
+  url: string
+  variableConfiguration: object
+}
+
+const httpResponseCallback = (res: NextApiResponse, endpoint: string) => (response) => {
+  let data = []
+  const headerDate =
+    response.headers && response.headers.date
+      ? response.headers.date
+      : "no response date"
+  console.log("Status Code:", response.statusCode)
+  console.log("Date in Response header:", headerDate)
+
+  response.on("data", (chunk) => {
+    res.send(chunk)
+    data.push(chunk)
+  })
+
+  response.on("end", () => {
+    response.destroy()
+    console.log(Buffer.concat(data).toString())
+    res.end(
+      `Response from ${endpoint} endpoint has ended: ${Buffer.concat(data).toString()}`
+    )
+  })
+}
