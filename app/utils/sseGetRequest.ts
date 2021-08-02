28465c10bc9e311f954f9483a853050714053203
Peter Ferguson <peterferguson95@gmail.com>
2021-08-02 11:45:44 +0100

    fixed https bug

--- app/utils/sseGetRequest.ts
+++ app/utils/sseGetRequest.ts
@@ -1,42 +1,28 @@
-import https, { IncomingMessage } from "http";
-import { NextApiResponse } from "next";
+import { IncomingMessage } from "http"
+import https from "https"
 
 export const sseGetRequest = async (
   url: string,
   responseCallback: (res: IncomingMessage) => void
-) => {
-  const headers = {
-    Authorization: `Basic ${Buffer.from(
-      process.env.ALPACA_KEY + ":" + process.env.ALPACA_SECRET
-    ).toString("base64")}`,
-    "content-type": "text/event-stream",
-  };
-  https.get(url, { headers }, responseCallback).on("error", (err) => {
-    console.log("HTTP Request Error: ", err.message);
-  });
-};
+) =>
+  https
+    .get(
+      url,
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            process.env.ALPACA_KEY + ":" + process.env.ALPACA_SECRET
+          ).toString("base64")}`,
+          "content-type": "text/event-stream",
+        },
+      },
+      responseCallback
+    )
+    .on("error", (err) => {
+      console.log("HTTP Request Error: ", err.message)
+    })
+
 export interface BaseServer {
-  url: string;
-  variableConfiguration: object;
+  url: string
+  variableConfiguration: object
 }
-export const httpResponseCallback = (res: NextApiResponse, endpoint: string) => (response) => {
-  let data = [];
-  const headerDate = response.headers && response.headers.date
-    ? response.headers.date
-    : "no response date";
-  console.log("Status Code:", response.statusCode);
-  console.log("Date in Response header:", headerDate);
-
-  response.on("data", (chunk) => {
-    res.send(chunk);
-    data.push(chunk);
-  });
-
-  response.on("end", () => {
-    response.destroy();
-    console.log(Buffer.concat(data).toString());
-    res.end(
-      `Response from ${endpoint} endpoint has ended: ${Buffer.concat(data).toString()}`
-    );
-  });
-};
