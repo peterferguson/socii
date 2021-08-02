5d7934056359eda68fdb17c6eef2db9cfdbfa3bc
Peter Ferguson <peterferguson95@gmail.com>
2021-08-02 19:57:48 +0100

    fix logo in sidebar

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import Logo from "@components/Logo"
+import Logo, { LogoAlone } from "@components/Logo"
 import { useAuth } from "@hooks/useAuth"
 import Link from "next/link"
 import { NextRouter, useRouter } from "next/router"
@@ -12,6 +12,48 @@ import {
   HiOutlineCog,
   HiOutlineUserGroup,
 } from "react-icons/hi"
+import { useMediaQuery } from "react-responsive"
+
+// - https://www.tailwind-kit.com/components/sidebar
+export default function Sidebar() {
+  // TODO: Stateful selection of the nav item based on the route
+  // - For now this is mocked with a simple enumeration of the list
+  const router = useRouter()
+  const { username } = useAuth()
+  const is2Col = !useMediaQuery({ minWidth: 1024 })
+
+  return (
+    <div className="sticky hidden w-20 h-screen mx-1 shadow-lg top-2 left-2 sm:block lg:w-52">
+      <div className="h-full bg-white rounded-2xl dark:bg-gray-700">
+        <div className="flex items-center justify-center pt-6">
+          {!is2Col ? (
+            <Logo className="text-2xl" />
+          ) : (
+            <LogoAlone height="32px" width="32px" />
+          )}
+        </div>
+        <nav className="mt-6">
+          {navItems(router, username).map((item) => (
+            <Link href={item.href} key={`${item.name}-selector`}>
+              <a
+                className={`flex items-center justify-start w-full p-4 my-2 font-thin uppercase transition-colors duration-200 ${
+                  item.isActive
+                    ? "text-brand-cyan-vivid border-r-4 border-brand bg-gradient-to-r from-white to-brand-light dark:from-gray-700 dark:to-gray-800"
+                    : "text-brand-shade-darkest dark:text-gray-200 hover:text-brand-cyan"
+                }`}
+              >
+                <item.icon className="mx-auto text-xl lg:mx-0" />
+                <span className="hidden mx-4 text-sm font-normal lg:inline-flex">
+                  {item.name}
+                </span>
+              </a>
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </div>
+  )
+}
 
 const navItems = (router: NextRouter, username: string) => [
   {
@@ -70,39 +112,3 @@ const navItems = (router: NextRouter, username: string) => [
     isActive: router.asPath.includes("settings"),
   },
 ]
-
-// - https://www.tailwind-kit.com/components/sidebar
-export default function Sidebar() {
-  // TODO: Stateful selection of the nav item based on the route
-  // - For now this is mocked with a simple enumeration of the list
-  const router = useRouter()
-  const { username } = useAuth()
-
-  return (
-    <div className="sticky hidden w-20 h-screen mx-1 shadow-lg top-2 left-2 sm:block lg:w-52">
-      <div className="h-full bg-white rounded-2xl dark:bg-gray-700">
-        <div className="flex items-center justify-center pt-6">
-          <Logo className="text-2xl" />
-        </div>
-        <nav className="mt-6">
-          {navItems(router, username).map((item) => (
-            <Link href={item.href} key={`${item.name}-selector`}>
-              <a
-                className={`flex items-center justify-start w-full p-4 my-2 font-thin uppercase transition-colors duration-200 ${
-                  item.isActive
-                    ? "text-brand-cyan-vivid border-r-4 border-brand bg-gradient-to-r from-white to-brand-light dark:from-gray-700 dark:to-gray-800"
-                    : "text-brand-shade-darkest dark:text-gray-200 hover:text-brand-cyan"
-                }`}
-              >
-                <item.icon className="mx-auto text-xl lg:mx-0" />
-                <span className="hidden mx-4 text-sm font-normal lg:inline-flex">
-                  {item.name}
-                </span>
-              </a>
-            </Link>
-          ))}
-        </nav>
-      </div>
-    </div>
-  )
-}
