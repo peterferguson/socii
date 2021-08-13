ef4e3bde08803a20fb47be954ae84673b4c60b96
Peter Ferguson <peterferguson95@gmail.com>
2021-08-13 18:46:50 +0100

    add padding to slider card

--- app/components/AssetCard.jsx
+++ app/components/AssetCard.jsx
@@ -9,9 +9,9 @@ export const AssetCard = ({ tickerSymbol, isin, shortName, price }) => (
       <TickerLogo tickerSymbol={tickerSymbol} isin={isin} />
     </header>
     <Link href={`/stocks/${tickerSymbol}`}>
-      <a className=" py-8 mx-3 grid grid-cols-none">
-        <p className="p-2 text-lg tracking-tight font-primary">{shortName}</p>
-        <h1 className="text-xl tracking-wider font-bold">{tickerSymbol}</h1>
+      <a className="py-8 mx-3 grid grid-cols-none">
+        <p className="py-1 text-lg tracking-tight font-primary">{shortName}</p>
+        <h1 className="py-1 text-xl font-bold tracking-wider">{tickerSymbol}</h1>
         <div>
           <div className={`font-bold text-3xl ${pnlTextColor(price?.changePercent)}`}>
             ${price?.iexRealtimePrice?.toFixed(2)}
