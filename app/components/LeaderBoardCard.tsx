568f83c6900262db4496beb70750f4e3305ae754
Peter Ferguson <peterferguson95@gmail.com>
2021-08-17 15:04:25 +0100

    working initial leaderboard page

--- app/components/LeaderBoardCard.tsx
+++ app/components/LeaderBoardCard.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React from "react"
 
-export const LeaderBoardCard = ({ rank }) => (
+export interface Leader {
+  groupName: string
+  portfolioValue: number
+  portfolioBreakdown: { [tickerSymbol: string]: number }[]
+  "%pnl": number
+}
+
+export const LeaderBoardCard = ({ rank, leader }: { rank: number; leader: Leader }) => (
   <article className="flex items-center justify-between p-6 uppercase bg-white shadow-md rounded-2xl">
     <div className="flex">
       <button className="flex flex-col px-8 py-4 font-bold bg-gray-100 border border-gray-300 rounded">
         <span role="img" aria-label="up arrow">
           🔝
         </span>
-        <span>{rank}</span>
+        <span>{rank + 1}</span>
       </button>
       <div className="flex flex-col justify-between ml-4">
         <div className="mb-4">
-          <p className="text-sm font-bold">GroupName</p>
+          <p className="text-sm font-bold">{leader.groupName}</p>
           <p>City</p>
         </div>
         <div className="flex items-center">
@@ -20,28 +27,32 @@ export const LeaderBoardCard = ({ rank }) => (
             <img
               className="inline-block w-6 h-6 rounded-full ring-2 ring-white"
               src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt="" />
+              alt=""
+            />
             <img
               className="inline-block w-6 h-6 rounded-full ring-2 ring-white"
               src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt="" />
+              alt=""
+            />
             <img
               className="inline-block w-6 h-6 rounded-full ring-2 ring-white"
               src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-              alt="" />
+              alt=""
+            />
             <img
               className="inline-block w-6 h-6 rounded-full ring-2 ring-white"
               src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt="" />
+              alt=""
+            />
           </div>
         </div>
       </div>
     </div>
     <div className="flex flex-col justify-between ml-4">
       <div className="mb-4 tracking-wider font-primary">
-        <p className="text-teal-400">% GAIN</p>
+        <p className="text-teal-400">{leader["%pnl"].toFixed(2)}%</p>
         <p className="text-sm">% Gain Pos</p>
       </div>
     </div>
   </article>
-);
+)
--- app/pages/groups/index.tsx
+++ app/pages/groups/index.tsx
@@ -1,31 +1,35 @@
 import React from "react"
-import { LeaderBoardCard } from "@components/LeaderBoardCard"
+import { Leader, LeaderBoardCard } from "@components/LeaderBoardCard"
 
 export default function GroupsHome({ leaders }) {
   return (
     <main className="w-full h-screen">
       <section>
         <div className="max-w-4xl mx-4 mt-20 lg:mx-auto space-y-4">
-          <h1 className="mb-8 font-bold tracking-wide uppercase">Leaderboard</h1>
-          {[1, 2, 3].map((rank) => (
-            <LeaderBoardCard key={`leader-row-${rank}`} rank={rank} />
+          <h1 className="mb-8 text-3xl font-bold tracking-wide uppercase font-primary">
+            Leaderboard
+          </h1>
+          <span className="text-sm font-primary">
+            The leaderboard is a list of the top performing groups on socii.
+            <br />
+            <br />
+            <p>How does your group compare against other sociians?</p>
+            <br />
+            <br />
+          </span>
+
+          {leaders.map((leader, rank) => (
+            <LeaderBoardCard key={`leader-row-${rank}`} rank={rank} leader={leader} />
           ))}
+        <p className="mt-8 font-primary text-tiny">
+          All gains shown reflect the month to date performane of each group. Only
+          public groups are displayed
+        </p>
         </div>
       </section>
     </main>
   )
 }
-
-// const rp = require('request-promise');
-
-// let getData = function(stockTicker) {
-// return(rp('https://www.styvio.com/api/' + stockTicker))
-// .then(body =>
-// console.log((JSON.parse(body))["ticker"] + " - " + (Object.keys(JSON.parse(body))))
-// )
-// };
-
-// getData("AAPL")
 
 export async function getStaticProps() {
   const getLeaderBoardProps = async () => {
@@ -35,8 +39,6 @@ export async function getStaticProps() {
     // * The timescale will be since the  beginning of the month.
     // * On the first of the month, the leaderboard is reset.
 
-    const CalendarApi = require("@alpaca/index").CalendarApi
-    const alpacaConfig = require("@alpaca/index").config
     const firestore = require("@lib/firebase/server/firebase-admin").firestore
     const functionUrl =
       "https://europe-west2-sociiinvest.cloudfunctions.net/get_historical_prices"
@@ -45,14 +47,26 @@ export async function getStaticProps() {
 
     const snapshot = await query.get()
 
-    const groupHoldings: { groupName: string; holdings: { tickerSymbol: string } }[] =
-      snapshot.docs.map((doc) => ({
+    const groupHoldings: {
+      [groupName: string]: { holdings: [{ tickerSymbol: string; qty: number }] }
+    } = snapshot.docs
+      .map((doc) => ({
         groupName: doc.ref.path.split("/")[1],
         holdings: doc.data(),
       }))
+      .reduce((initial, item) => {
+        if (item.groupName in initial)
+          initial[item.groupName].holdings.push(item.holdings)
+        else initial[item.groupName] = { holdings: [item.holdings] }
+        return initial
+      }, {})
 
     const tickers: string[] = [
-      ...new Set(groupHoldings.map((group) => group.holdings.tickerSymbol)),
+      ...new Set(
+        Object.values(groupHoldings)
+          .map(({ holdings }) => [...holdings.map((holding) => holding.tickerSymbol)])
+          .flat(1)
+      ),
     ]
 
     // - Get the number of market days since the beginning of the month.
@@ -79,39 +93,69 @@ export async function getStaticProps() {
       if (symbol in data) data[symbol].push({ close, timestamp: new Date(timestamp) })
       else
         Object.assign(data, { [symbol]: [{ close, timestamp: new Date(timestamp) }] })
-      console.log(data)
-
       return data
     }, {})
 
     // - monthly pct change lagging by one day
     // ! latest data is the close of the previous market day
     // ! so if the market is closed the currentPrice will not the last close price but the prev day close price
-    // const tickerPriceChanges = tickers.reduce((data, ticker) => {
-    //   const prices = priceData[ticker]
+    const tickerPriceChanges: { [tickerSymbol: string]: number } = tickers.reduce(
+      (data, ticker) => {
+        const prices = priceData[ticker]
 
-    //   const pctChange =
-    //     (100 * (prices[prices.length - 1].close - prices[0].close)) / prices[0].close
+        const pctChange =
+          (100 * (prices[prices.length - 1].close - prices[0].close)) / prices[0].close
 
-    //   return Object.assign(data, { [ticker]: pctChange })
-    // }, {})
-    // // TODO: Get best performer from this data for each group!
-
-    // const portfolioSplit =
-
-    console.log(
-      groupHoldings.map(({ holdings }) => {
-        console.log(priceData)
-
-        return priceData[holdings.tickerSymbol]
-      })
-    )
-
-    return {
-      props: {
-        leaders: null,
+        return Object.assign(data, { [ticker]: pctChange })
       },
+      {}
+    )
+    // TODO: Get best performer from this data for each group!
+
+    // - get the holding breakdown for each group
+    const portfolioSplits: {
+      [groupName: string]: {
+        portfolioValue: number
+        portfolioBreakdown: { [tickerSymbol: string]: number }[]
+        "%pnl": number
+      }
+    } = Object.entries(groupHoldings).reduce((initial, [groupName, { holdings }]) => {
+      const portfolioValue = holdings
+        ?.map(
+          ({ tickerSymbol, qty }) => priceData?.[tickerSymbol].slice().pop().close * qty
+        )
+        .reduce((a, b) => a + b, 0)
+
+      const portfolioBreakdown = holdings.reduce(
+        (initial, { tickerSymbol, qty }) =>
+          Object.assign(initial, {
+            [tickerSymbol]:
+              (100 * (priceData?.[tickerSymbol].slice().pop().close * qty)) /
+              portfolioValue,
+          }),
+        {}
+      )
+      return Object.assign(initial, {
+        [groupName]: { portfolioValue, portfolioBreakdown },
+      })
+    }, {})
+
+    // - Get %pnl for each group
+    for (const [groupName, split] of Object.entries(portfolioSplits)) {
+      portfolioSplits[groupName]["%pnl"] = Object.entries(tickerPriceChanges)
+        .map(([ticker, pctChange]) =>
+          ticker in split.portfolioBreakdown
+            ? 0.01 * pctChange * split.portfolioBreakdown[ticker]
+            : 0
+        )
+        .reduce((a, b) => a + b)
     }
+
+    const leaders: Leader[] = Object.entries(portfolioSplits)
+      .map(([groupName, split]) => ({ groupName, ...split }))
+      .sort((a, b) => b[Object.keys(b)[0]]["%pnl"] - a[Object.keys(a)[0]]["%pnl"])
+
+    return { props: { leaders } }
   }
   return await getLeaderBoardProps()
 }
