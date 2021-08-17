4b383e345abdfd4746c65f0b93829e07070b948a
Peter Ferguson <peterferguson95@gmail.com>
2021-08-17 13:54:12 +0100

    adding leaderboard price & holding queries

--- app/pages/groups/index.tsx
+++ app/pages/groups/index.tsx
@@ -29,39 +29,83 @@ export default function GroupsHome({ leaders }) {
 
 export async function getStaticProps() {
   const getLeaderBoardProps = async () => {
-    // const response = await fetch("https://www.styvio.com/api/leaderboard")
-    // const data = await response.json()
+    // * The leaderboard will be a lagging month to date list of the top 10 groups.
+    // * Lagging by a single day.
+    // * The leaderboard will be updated every day.
+    // * The timescale will be since the  beginning of the month.
+    // * On the first of the month, the leaderboard is reset.
 
-    const db = require("@lib/firebase/server/firebase-admin").firestore
+    const CalendarApi = require("@alpaca/index").CalendarApi
+    const alpacaConfig = require("@alpaca/index").config
+    const firestore = require("@lib/firebase/server/firebase-admin").firestore
+    const functionUrl =
+      "https://europe-west2-sociiinvest.cloudfunctions.net/get_historical_prices"
 
-    const query = db.collectionGroup("holdings")
+    const query = firestore.collectionGroup("holdings")
 
     const snapshot = await query.get()
 
-    const groupHoldings = snapshot.docs.map((doc) => ({
-      groupName: doc.ref.path.split("/")[1],
-      holdings: doc.data(),
-    }))
+    const groupHoldings: { groupName: string; holdings: { tickerSymbol: string } }[] =
+      snapshot.docs.map((doc) => ({
+        groupName: doc.ref.path.split("/")[1],
+        holdings: doc.data(),
+      }))
 
-    const tickers = [
+    const tickers: string[] = [
       ...new Set(groupHoldings.map((group) => group.holdings.tickerSymbol)),
     ]
 
-    const data = Promise.all(
-      tickers.map(
-        async (ticker) =>
-          await (await fetch(`https://www.styvio.com/api/${ticker}`)).json()
-      )
+    // - Get the number of market days since the beginning of the month.
+    // - get the first day of this month
+    const today = new Date()
+
+    const todayString = today.toISOString().slice(0, 10)
+    const firstDayString = todayString.slice(0, 8) + "01"
+
+    const yahooData = await (
+      await fetch(functionUrl, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          tickerSymbol: tickers.join(" "),
+          start: firstDayString,
+          end: todayString,
+        }),
+      })
+    ).json()
+
+    const priceData = yahooData.reduce((data, tick) => {
+      const { symbol, close, timestamp } = tick
+      if (symbol in data) data[symbol].push({ close, timestamp: new Date(timestamp) })
+      else
+        Object.assign(data, { [symbol]: [{ close, timestamp: new Date(timestamp) }] })
+      console.log(data)
+
+      return data
+    }, {})
+
+    // - monthly pct change lagging by one day
+    // ! latest data is the close of the previous market day
+    // ! so if the market is closed the currentPrice will not the last close price but the prev day close price
+    // const tickerPriceChanges = tickers.reduce((data, ticker) => {
+    //   const prices = priceData[ticker]
+
+    //   const pctChange =
+    //     (100 * (prices[prices.length - 1].close - prices[0].close)) / prices[0].close
+
+    //   return Object.assign(data, { [ticker]: pctChange })
+    // }, {})
+    // // TODO: Get best performer from this data for each group!
+
+    // const portfolioSplit =
+
+    console.log(
+      groupHoldings.map(({ holdings }) => {
+        console.log(priceData)
+
+        return priceData[holdings.tickerSymbol]
+      })
     )
-
-    const pctChange = (await data).map(({ ticker, oneMonthPrices }) => ({
-      ticker,
-      pctChange:
-        (100 * (oneMonthPrices[oneMonthPrices.length - 1] - oneMonthPrices[0])) /
-        oneMonthPrices[oneMonthPrices.length - 1],
-    }))
-
-    console.log(pctChange)
 
     return {
       props: {
