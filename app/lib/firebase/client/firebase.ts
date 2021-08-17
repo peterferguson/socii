083067f034cba90510a379a50a5064bd418bd2c3
Peter Ferguson <peterferguson95@gmail.com>
2021-08-17 23:19:06 +0100

    update firebase-admin config

--- app/lib/firebase/client/firebase.ts
+++ app/lib/firebase/client/firebase.ts
@@ -6,7 +6,7 @@ export function initialize() {
   const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
     authDomain: "sociiinvest.firebaseapp.com",
-    projectId: "sociiinvest",
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: "sociiinvest.appspot.com",
     messagingSenderId: "584929113403",
     appId: "1:584929113403:web:73fa9920cb14c1cc19b31e",
--- app/lib/firebase/server/firebase-admin.ts
+++ app/lib/firebase/server/firebase-admin.ts
@@ -1,15 +1,14 @@
 import admin from "firebase-admin"
 
-const config = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
-  appId: process.env.FIREBASE_APP_ID,
-  authDomain: "sociiinvest.firebaseapp.com",
-  projectId: "sociiinvest",
-  storageBucket: "sociiinvest.appspot.com",
-}
-
 // * Constant initialisation
-if (!admin.apps.length) admin.initializeApp(config)
+if (!admin.apps.length)
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+  })
 
 const firestore = admin.firestore()
 const auth = admin.auth()
@@ -19,7 +18,5 @@ const serverTimestamp = admin.firestore.FieldValue.serverTimestamp
 const fromDate = admin.firestore.Timestamp.fromDate
 
 const DocumentReference = typeof admin.firestore.DocumentReference
-
-export type Firestore = typeof admin.firestore.Firestore
 
 export { firestore, auth, arrayUnion, fromDate, serverTimestamp, DocumentReference }
