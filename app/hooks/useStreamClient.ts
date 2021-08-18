545f5292be0389fd520a0ff847e2caf63e221f81
Peter Ferguson <peterferguson95@gmail.com>
2021-08-18 01:13:46 +0100

    fix type issue

--- app/hooks/useStreamClient.ts
+++ app/hooks/useStreamClient.ts
@@ -15,7 +15,7 @@ export const useStreamClient = () => {
     // TODO: Refactor the data model and have a public user_portfolio collection & private user subcollection with keys for each user
     const connectStreamUser = async () => {
       console.log(`Connecting to stream for user ${user.uid}`)
-      
+
       if (user?.streamToken && process.env.NODE_ENV === "development") {
         await streamClient.current?.connectUser(
           { id: username, name: user.displayName },
@@ -37,7 +37,7 @@ export const useStreamClient = () => {
                 userId: username,
               }),
             })
-            return response.json()?.token
+            return (await response.json())?.token
           }
         )
       }
