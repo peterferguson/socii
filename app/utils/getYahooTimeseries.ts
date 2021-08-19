8458606650bbe1b158a66fe609e118a27042992b
Peter Ferguson <peterferguson95@gmail.com>
2021-08-19 11:13:26 +0100

    add missing yahoo data util

---
+++ app/utils/getYahooTimeseries.ts
@@ -0,0 +1,89 @@
+import { DateStr } from "@models/DateStr"
+
+export enum PeriodEnum {
+  "1d",
+  "5d",
+  "7d",
+  "60d",
+  "1mo",
+  "3mo",
+  "6mo",
+  "1y",
+  "2y",
+  "5y",
+  "10y",
+  "ytd",
+  "max",
+}
+
+export enum IntervalEnum {
+  "1m",
+  "2m",
+  "5m",
+  "15m",
+  "30m",
+  "60m",
+  "90m",
+  "1h",
+  "1d",
+  "5d",
+  "1wk",
+  "1mo",
+  "3mo",
+}
+
+interface YahooTimeseriesTick {
+  timestamp: Date
+  high: number
+  volume: number
+  close: number
+  low: number
+  open: number
+  adjclose: number
+  dividends: number
+}
+
+export interface YahooTimeseries {
+  [tickerSymbol: string]: YahooTimeseriesTick[]
+}
+
+interface getYahooTimeseriesProps {
+  tickers: string[]
+  startDateStr?: DateStr
+  endDateStr?: DateStr
+  period?: PeriodEnum
+  interval?: IntervalEnum
+}
+
+export const getYahooTimeseries = async ({
+  tickers,
+  startDateStr,
+  endDateStr,
+  period,
+  interval,
+}: getYahooTimeseriesProps): Promise<YahooTimeseries> => {
+  const functionUrl =
+    "https://europe-west2-sociiinvest.cloudfunctions.net/get_historical_prices"
+
+  const yahooData = await (
+    await fetch(functionUrl, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        period,
+        interval,
+        tickerSymbol: tickers.join(" "),
+        start: startDateStr,
+        end: endDateStr,
+      }),
+    })
+  ).json()
+
+  return yahooData?.reduce((data, tick) => {
+    const { symbol, timestamp, ...ohlcv } = tick
+    if (symbol in data) data[symbol].push({ ...ohlcv, timestamp: new Date(timestamp) })
+    else
+      Object.assign(data, { [symbol]: [{ ...ohlcv, timestamp: new Date(timestamp) }] })
+    return data
+  }, {})
+}
