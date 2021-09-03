fc1170f2d7af66c6bc934a998eff208292cb1e52
jamesmcc5 <james.mcc94@hotmail.co.uk>
2021-09-03 02:30:49 +0200

    skeleton for tabs on groups page

--- app/pages/groups/index.tsx
+++ app/pages/groups/index.tsx
@@ -1,46 +1,103 @@
-import React from "react"
+// TODO: A lot. Basic skeleton for tabed group page complete 
+//              but including some very temp placeholders
+//      
+//
+// - In process of making code less awful:
+//     - make a seperate component for creating the tab headers / panels
+//       to reduce repeted code and make more flexible to changes
+//     - actually link to correct groups/leaderboards components
+//     - link to real route
+
+// - Potential:
+//   - add "followed groups" page to check status of other open groups
+
+
+import React , { Fragment , useState} from "react"
 import LeaderBoardCard from "@components/LeaderBoardCard"
-import { getLeaderBoardProps } from "../../utils/getLeaderBoardProps"
+//import { getLeaderBoardProps } from "../../utils/getLeaderBoardProps"
 import { useAuth } from "@hooks"
+import { Tab } from '@headlessui/react'
+import { setUserState } from "@lib/firebase/client/db"
+import Group from "./[groupName]/index"
+
 
 const GroupsHome = ({ leaders }) => {
   const { userGroups } = useAuth()
 
+const [selectedTab , setSelectedTab] = useState("")
+
   return (
-    <main className="w-full h-screen">
-      <section className="flex flex-col justify-between ">
-        <div className="max-w-4xl mx-4 mt-12 sm:mt-20 md:mx-auto space-y-4">
-          <h1 className="mb-8 text-3xl font-bold tracking-wide uppercase font-primary">
-            Leaderboard
-          </h1>
-          <span className="text-sm font-primary">
-            The leaderboard is a list of the top performing groups on socii.
-            <br />
-            <br />
-            <p>How does your group compare against other sociians?</p>
-            <br />
-            <br />
-          </span>
-          {leaders?.map((leader, rank) => (
-            <LeaderBoardCard
-              key={`leader-${rank}-${leader.groupName}`}
-              rank={rank}
-              leader={
-                userGroups.includes(leader.groupName)
-                  ? { inGroup: true, ...leader }
-                  : leader
-              }
-            />
-          ))}
-          <p className="mt-8 font-primary text-tiny">
-            All gains shown reflect the month to date performance of each group. Only
-            public groups are displayed
-          </p>
+    
+    <Tab.Group>
+      <div className = "container flex flex-col center" > 
+        <div className="flex flex-row justify-center bg-white rounded-lg shadow-lg font-primary text-l divide-x-2 divide-blue-500">
+          <Tab.List>
+            <Tab>
+              <button 
+              onClick={() => setSelectedTab("My Groups")} 
+              className = {"px-5 py-5 bg-gray-100 "+( (selectedTab==="My Groups") ? "font-bold border-b-2 border-blue-500" : "") +"hover:border-b-2 border-blue-500"}>
+                My Groups 
+              </button>
+            </Tab>
+            <Tab>
+              <button 
+              onClick={() => setSelectedTab("Leaderboards")} 
+              className = {"px-5 py-5 bg-gray-100 "+( (selectedTab==="Leaderboards") ? "font-bold border-b-2 border-blue-500" : "") +"hover:border-b-2 border-blue-500"}>
+                Leaderboards
+              </button>
+            </Tab>
+          </Tab.List>
         </div>
-      </section>
-    </main>
+        <Tab.Panels>
+          <div className="flex justify-center">
+            <div className= " flex-col justify-center w-10/12 bg-gray-300 rounded-lg shadow-lg">
+              <Tab.Panel><Group/></Tab.Panel>
+
+              <Tab.Panel>
+                <section className="flex flex-col justify-between ">
+                  <div className="max-w-4xl mx-4 mt-12 sm:mt-20 md:mx-auto space-y-4">
+                    <h1 className="mb-8 text-3xl font-bold tracking-wide uppercase font-primary">
+                      Leaderboard
+                    </h1>
+                    <span className="text-sm font-primary">
+                      The leaderboard is a list of the top performing groups on socii.
+                      <br />
+                      <br />
+                      <p>How does your group compare against other sociians?</p>
+                      <br />
+                      <br />
+                    </span>
+                    {leaders?.map((leader, rank) => (
+                      <LeaderBoardCard
+                        key={`leader-${rank}-${leader.groupName}`}
+                        rank={rank}
+                        leader={
+                          userGroups.includes(leader.groupName)
+                            ? { inGroup: true, ...leader }
+                            : leader
+                        }
+                      />
+                    ))}
+                    <p className="mt-8 font-primary text-tiny">
+                      All gains shown reflect the month to date performance of each group. Only
+                      public groups are displayed
+                    </p>
+                  </div>
+                </section>
+              </Tab.Panel>
+
+            </div>
+          </div>
+          </Tab.Panels>
+      </div>
+    </Tab.Group>
+
+
   )
 }
 export default GroupsHome
 
-export const getStaticProps = async () => await getLeaderBoardProps()
+//export const getStaticProps = async () => await getLeaderBoardProps()
+
+
+
