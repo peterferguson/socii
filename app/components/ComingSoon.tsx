00bb209db77dc77c751fcc374cd03286a2b1f8e0
Peter Ferguson <peterferguson95@gmail.com>
2021-09-18 10:37:40 +0100

    crypto page styling fix

--- app/components/ComingSoon.tsx
+++ app/components/ComingSoon.tsx
@@ -10,7 +10,7 @@ interface IComingSoon {
 
 export default function ComingSoon({ children, color, description = "" }: IComingSoon) {
   return (
-    <div className="flex flex-col w-full h-full mt-0 bg-gray-50 sm:mt-8">
+    <div className="flex flex-col mt-0 bg-gray-50 sm:mt-8">
       <div
         className={tw(
           "w-4/5 p-10 mx-auto my-20 text-center border-4 shadow-2xl cursor-pointer grid place-items-center sm:my-auto",
@@ -21,21 +21,21 @@ export default function ComingSoon({ children, color, description = "" }: IComin
         {children}
         <h1
           className={tw(
-            `text-4xl font-bold uppercase text-${color} transition duration-500`
+            `text-2xl sm:text-4xl font-bold uppercase text-${color} transition duration-500`
           )}
         >
           Coming Soon
         </h1>
         {description && (
-          <h2 className="text-xl text-gray-700 transition duration-500">
+          <h2 className="text-xs text-gray-700 sm:text-xl transition duration-500">
             {description}
           </h2>
         )}
-        <h2 className="text-xl text-gray-700 transition duration-500">
+        <h2 className="text-xs text-gray-700 sm:text-xl transition duration-500">
           We have many projects on our roadmap at the moment. Which you can check out
           here & vote on what you would like to see the most!
         </h2>
-        <h2 className="text-xl text-gray-700 transition duration-500">
+        <h2 className="text-xs text-gray-700 sm:text-xl transition duration-500">
           While you wait check us out on your favorite social network.
         </h2>
         <div className="grid grid-cols-3 gap-2 md:gap-4">
@@ -43,21 +43,21 @@ export default function ComingSoon({ children, color, description = "" }: IComin
             // href="https://www.facebook.com/socii"
             title="socii On Facebook"
           >
-            <FaFacebook className="w-24 h-24 px-6 py-2 font-bold tracking-wide hover:text-white hover:bg-facebook text-facebook transition duration-500" />
+            <FaFacebook className="w-16 sm:w-24 h-16 sm:h-24 px-1 sm:px-6 py-2 font-bold tracking-wide hover:text-white hover:bg-facebook text-facebook transition duration-500" />
           </a>
 
           <a
             // href="https://twitter.com/socii/"
             title="socii On Twitter"
           >
-            <FaTwitter className="w-24 h-24 px-6 py-2 font-bold tracking-wide hover:text-white hover:bg-twitter text-twitter transition duration-500" />
+            <FaTwitter className="w-16 sm:w-24 h-16 sm:h-24 px-1 sm:px-6 py-2 font-bold tracking-wide hover:text-white hover:bg-twitter text-twitter transition duration-500" />
           </a>
 
           <a
             // href="https://www.medium.com/user/socii/"
             title="socii On Medium"
           >
-            <FaMedium className="w-24 h-24 px-6 py-2 font-bold tracking-wide hover:text-white hover:bg-black transition duration-500" />
+            <FaMedium className="w-16 sm:w-24 h-16 sm:h-24 px-1 sm:px-6 py-2 font-bold tracking-wide hover:text-white hover:bg-black transition duration-500" />
           </a>
         </div>
       </div>
--- app/pages/crypto/index.tsx
+++ app/pages/crypto/index.tsx
@@ -2,36 +2,36 @@ import ComingSoon from "@components/ComingSoon"
 import React from "react"
 import { FaBitcoin } from "react-icons/fa"
 
-import { useAuth } from "@hooks/useAuth"
+// import { useAuth } from "@hooks/useAuth"
 
-const url = "ws://server.socii.app/api/v1/alpaca/events/trades/stream"
+// const url = "ws://server.socii.app/api/v1/alpaca/events/trades/stream"
 
 // ! In tailwind jit compile the code is scanned and the color must be explicit
 // ! so we need the following text-bitcoin bg-bitcoin border-bitcoin
 const CryptoHome = ({ color = "bitcoin" }) => {
-  const { user } = useAuth()
-  const ws = React.useRef<WebSocket>(null)
+  // const { user } = useAuth()
+  // const ws = React.useRef<WebSocket>(null)
 
-  React.useEffect(() => {
-    if (user?.token) {
-      ws.current = new WebSocket(url + `?token=${user?.token}`)
+  // React.useEffect(() => {
+  //   if (user?.token) {
+  //     ws.current = new WebSocket(url + `?token=${user?.token}`)
 
-      ws.current.onopen = () => console.log("ws opened")
-      ws.current.onclose = () => console.log("ws closed")
+  //     ws.current.onopen = () => console.log("ws opened")
+  //     ws.current.onclose = () => console.log("ws closed")
 
-      ws.current.onmessage = (e) => {
-        console.log("ws.current.onmessage", e)
-        try {
-          const data = JSON.parse(e.data)
-          console.log(data)
-        } catch (error) {
-          const message = e.data
-          console.log("e", message)
-        }
-      }
-      return () => ws.current.close()
-    }
-  }, [user?.token])
+  //     ws.current.onmessage = (e) => {
+  //       console.log("ws.current.onmessage", e)
+  //       try {
+  //         const data = JSON.parse(e.data)
+  //         console.log(data)
+  //       } catch (error) {
+  //         const message = e.data
+  //         console.log("e", message)
+  //       }
+  //     }
+  //     return () => ws.current.close()
+  //   }
+  // }, [user?.token])
 
   return (
     <ComingSoon
@@ -40,7 +40,7 @@ const CryptoHome = ({ color = "bitcoin" }) => {
       diversity. That is why we are pushing to bring crypto to socii as quickly as
       possible"
     >
-      <FaBitcoin className={`w-24 h-24 text-${color}`} />
+      <FaBitcoin className={`w-16 sm:w-24 h-16 sm:h-24 text-${color}`} />
     </ComingSoon>
   )
 }
