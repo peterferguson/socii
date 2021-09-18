b1a2a70fb9675f3249c5edb9c8ef8efc6e17ce84
Peter Ferguson <peterferguson95@gmail.com>
2021-09-18 11:22:44 +0100

    add user profile loader

--- app/components/UserPhoto.tsx
+++ app/components/UserPhoto.tsx
@@ -1,19 +1,31 @@
 import { useAuth } from "@hooks"
-import { getRandomTailwindColor } from "@utils/getRandomTailwindColor"
+import { getInitials } from "@utils/getInitials"
+import { getRandomImage } from "@utils/getRandomImage"
+import { tw } from "@utils/tw"
+import Image from "next/image"
 import React from "react"
 
-export default function UserPhoto({
-  className = "w-8 h-8 m-4 rounded-full",
-}) {
+interface IUserPhotoProps {
+  className?: string
+  width?: string
+  height?: string
+}
+
+const DEFAULT_HEIGHT_AND_WIDTH = "32px"
+
+const UserPhoto = ({ height, width, className }: IUserPhotoProps) => {
   const { user } = useAuth()
 
-  return !user?.photoUrl ? (
-    <div
-      className={`${className} flex items-center justify-center bg-${getRandomTailwindColor()} m-auto font-primary`}
-    >
-      {user?.displayName?.[0]}
-    </div>
+  return !user ? (
+    <div className="w-8 h-8 bg-gray-200 rounded-full animate-pulse" />
   ) : (
-    <img src={user?.photoUrl} className={`${className}`} />
+    <Image
+      src={user?.photoUrl || getRandomImage(getInitials(user?.displayName))}
+      className={tw("rounded-full", className)}
+      height={height || DEFAULT_HEIGHT_AND_WIDTH}
+      width={width || DEFAULT_HEIGHT_AND_WIDTH}
+    />
   )
 }
+
+export default UserPhoto
