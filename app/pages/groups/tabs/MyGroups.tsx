c2f79a6dcf4592dc46b89cad51d2e70516f6bf16
James McComish <james.mcc94@hotmail.co.uk>
2021-09-24 22:48:46 +0200

    corrected userGroups -> user?.groups

--- app/pages/groups/tabs/MyGroups.tsx
+++ app/pages/groups/tabs/MyGroups.tsx
@@ -1,18 +1,14 @@
 // TODO:
 // potentially add all the surrounding tab context to the page.... would this cause issues for certain non users?
 
-import React , { useMemo } from "react"
-import { useAuth } from "@hooks"
 import { GroupPortfolios } from "@components/GroupPortfolios"
-import { useRouter } from "next/router"
+import React, { useMemo } from "react"
 
 const MyGroups = () => {
-  const { userGroups } = useAuth()
-  const router = useRouter()
 
   const groupsCards:JSX.Element = useMemo(() => 
-    MyComp(userGroups)
-  ,[userGroups])
+    <GroupPortfolios/>
+  ,[])
   
   return (
     <div>
@@ -21,16 +17,3 @@ const MyGroups = () => {
   )
 }
 export default MyGroups
-
-export const MyComp = (userGroups: string[]) => (
-  <GroupPortfolios userGroupsList= {userGroups}/>
-)
-
-// // TODO fix paths
-// export async function getStaticPaths() {
-//     return {
-//       paths: [],
-//       fallback: "blocking",
-//     };
-//   }
-
