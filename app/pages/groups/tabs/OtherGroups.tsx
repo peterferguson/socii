9adf0ea1cc39d9f4c423b520b24c4b3acd15770c
Peter Ferguson <peterferguson95@gmail.com>
2021-09-24 21:56:28 +0100

    remove userGroups from auth

--- app/pages/groups/tabs/OtherGroups.tsx
+++ app/pages/groups/tabs/OtherGroups.tsx
@@ -1,21 +1,15 @@
 // TODO:
 // potentially add all the surrounding tab context to the page.... would this cause issues for certain non users?
 
-import React , { Fragment , useState , useMemo , useEffect} from "react"
-
-import { useAuth } from "@hooks"
 import ComingSoon from "@components/ComingSoon"
+import React from "react"
 import { FaUserInjured } from "react-icons/fa"
-import { useRouter } from "next/router"
 
 const OtherGroups = () => {
-    const { userGroups } = useAuth()
-    const router = useRouter()
-  
   return (
     <div>
       <ComingSoon color="brand-pink" description="">
-      <FaUserInjured className={`w-24 h-24 text-brand-pink`} />
+        <FaUserInjured className={`w-24 h-24 text-brand-pink`} />
       </ComingSoon>
     </div>
   )
@@ -29,4 +23,3 @@ export default OtherGroups
 //       fallback: "blocking",
 //     };
 //   }
-
