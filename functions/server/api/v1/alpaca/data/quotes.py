5b05c22f3bf5824aa8c1c24d6a592a80e8260190
Peter Ferguson <peterferguson95@gmail.com>
2021-09-29 12:59:55 +0100

    get realtime quotes & historical quotes from the server

--- functions/server/api/v1/alpaca/data/quotes.py
+++ functions/server/api/v1/alpaca/data/quotes.py
@@ -1,12 +1,19 @@
+import asyncio
 import logging
+import os
+import threading
+from typing import Any, List
 
-from fastapi import APIRouter, Path
-from fastapi.param_functions import Depends
+
+from alpaca_trade_api.common import URL
+from alpaca_trade_api.stream import Stream
+
 
 from core.connection_manager import ConnectionManager
+from fastapi import APIRouter
+from fastapi.param_functions import Depends
 from utils.get_data import get_data
 from utils.verify_token import verify_token
-
 
 logger = logging.getLogger("main")
 
@@ -14,9 +21,65 @@ router = APIRouter()
 
 manager = ConnectionManager()
 
+data_url = os.getenv("APCA_API_DATA_URL", "")
+
+feed = "iex"
+
+
+# def unsubscribe_handler(stream: Stream, data_type: str, symbols: List[str]):
+#     switch = {
+#         "quotes": stream.unsubscribe_quotes,
+#         "trades": stream.unsubscribe_trades,
+#         "bars": stream.unsubscribe_bars,
+#     }
+#     try:
+#         switch[data_type](*symbols)
+#     except KeyError:
+#         logger.error("handlers", stream._data_ws._handlers)
+#         logger.error("Invalid data type", data_type)
+#     except Exception as e:
+#         logger.error(e)
+
+
+def consumer_thread(data_type, symbols, data):
+    try:
+        # make sure we have an event loop, if not create a new one
+        loop = asyncio.get_event_loop()
+        loop.set_debug(True)
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    stream = Stream(data_stream_url=URL(data_url), data_feed=feed)
+
+    try:
+        loop.run_until_complete(get_data(stream, data_type, symbols, data))
+    finally:
+        # unsubscribe_handler(stream, data_type, symbols)
+        loop.stop()
+        loop.close()
+
 
 @router.get("/", dependencies=[Depends(verify_token)])
 async def get_quotes(
     symbols: str,  # Comma-separated list of symbols to subscribe to,
 ):
-    return await get_data(data_type="quotes", symbols=symbols.split(","))
+    """
+    Get quotes for a list of symbols.
+    """
+    logger.info(f"Getting quotes for {symbols}")
+    data: dict[str, dict] = {}
+    thread = threading.Thread(
+        target=consumer_thread, args=("quotes", symbols.split(","), data)
+    )
+
+    thread.start()
+
+    # await stream.unsubscribe_quotes(*symbols.split(","))
+
+    thread.join()
+
+    logger.info("Got data")
+    logger.info(data)
+
+    return data
--- functions/server/utils/get_data.py
+++ functions/server/utils/get_data.py
@@ -1,24 +1,65 @@
+import aiohttp
 import asyncio
-import json
 import os
+import requests
 import logging
 from typing import List
 
-from alpaca_trade_api.common import URL
 from alpaca_trade_api.stream import Stream
+from alpaca_trade_api.rest import REST
 
 logger = logging.getLogger("main")
 
-data_url = os.getenv("APCA_API_DATA_URL", "")
 
-logger.info(logging.INFO, "Getting data from Alpaca API")
+api_key = os.environ.get("APCA_API_KEY_ID", "")
+api_secret = os.environ.get("APCA_API_SECRET_KEY", "")
 
 
-async def get_data(data_type: str, symbols: List[str]):
+def get_market_time():
+    """
+    Get the current market time.
+    """
+    logger.info(os.environ.get("APCA_API_BASE_URL", "") + "clock")
+    market_time_request = requests.get(
+        os.environ.get("APCA_API_BASE_URL", "") + "clock",
+        auth=(api_key, api_secret),
+    )
+
+    logger.info(market_time_request.status_code)
+    return market_time_request.json()
+
+
+async def get_latest_quote(session, symbol):
+    """
+    Get the latest quote for a given symbol.
+    """
+    url = f"https://data.sandbox.alpaca.markets/v2/stocks/{symbol}/quotes/latest"
+    async with session.get(url) as resp:
+        logger.info(resp.status)
+        return await resp.json()
+
+
+async def get_latest_quotes(symbols):
+    """
+    Get the latest quote for a given list of symbols async.
+    """
+    logger.info(f"Getting latest quotes for {symbols} from Alpaca API")
+    async with aiohttp.ClientSession(
+        auth=aiohttp.BasicAuth(api_key, api_secret)
+    ) as session:
+
+        tasks = []
+        for symbol in symbols:
+            tasks.append(asyncio.ensure_future(get_latest_quote(session, symbol)))
+
+        return await asyncio.gather(*tasks)
+
+
+async def get_data(stream: Stream, data_type: str, symbols: List[str], data):
     """
     Get data from Alpaca API.
     """
-    logger.info("Getting data from Alpaca API")
+    logger.info(f"Getting {data_type} data for {symbols} from Alpaca API")
 
     quote_symbols = symbols if data_type == "quotes" else None
     trade_symbols = symbols if data_type == "trades" else None
@@ -26,45 +67,49 @@ async def get_data(data_type: str, symbols: List[str]):
     if not quote_symbols and not trade_symbols:
         return
 
-    feed = "iex"
-    stream = Stream(data_stream_url=URL(data_url), data_feed=feed)
+    market_time = get_market_time()
+    logger.info(f"Market time: {market_time}")
 
     if quote_symbols:
-        latest_quotes = {symbol: None for symbol in quote_symbols}
+
+        if not market_time["is_open"]:
+            logger.info("Market is closed, getting historical quote data")
+
+            latest = await get_latest_quotes(symbols)
+            for result in latest:
+                data[result["symbol"]] = result
+
+            return
+
+        # - continue until quotes for all symbols are received
+        logger.info("Waiting for data...")
 
         @stream.on_quote(*quote_symbols)
-        async def quote_handler(quote):
-            latest_quotes[quote.symbol] = quote._raw
+        async def _(quote):
+            data[quote.symbol] = quote._raw
             logger.info(quote)
+
+            # ---------------------------------------------------------------------------
+            # - Uncomment to stream quote data continuously with three seconds delay
+            # ---------------------------------------------------------------------------
+            # await asyncio.sleep(3)
+
             try:
                 stream.unsubscribe_quotes(quote.symbol)
             except RuntimeError:
                 #  ignore event loop is already running exception
                 pass
+            try:
+                stream.run()  # keep the stream alive
+            except RuntimeError:
+                #  ignore event loop is already running exception
+                pass
 
-            # ---------------------------------------------------------------------------
-            # - Uncomment to stream quote data continuously with three seconds delay
-            # ---------------------------------------------------------------------------
+            # stream.subscribe_crypto_trades(print_crypto_trade, "BTCUSD")
 
-            # try:
-            #     await stream.unsubscribe_quotes(quote.symbol)
-            # except RuntimeError:
-            #     #  ignore event loop is already running exception
-            #     pass
-            # await asyncio.sleep(3)
+        while any(symbol not in data.keys() for symbol in symbols):
+            # logger.info([symbol not in data.keys() for symbol in symbols])
+            # logger.info(data)
+            pass
 
-    try:
-        stream.run()  # keep the stream alive
-    except RuntimeError:
-        #  ignore event loop is already running exception
-        pass
-
-    # stream.subscribe_crypto_trades(print_crypto_trade, "BTCUSD")
-
-    # - continue until quotes for all symbols are received
-    logger.info("Waiting for data...")
-    logger.info(any(v == None for v in latest_quotes.values()))
-    while any(v == None for v in latest_quotes.values()):
-        await asyncio.sleep(0.1)
-    asyncio.gather(stream.stop_ws())
-    return latest_quotes
+    return
