16ebed7d0b95043f6079094681613f90ecdf2fd9
James McComish <james.mcc94@hotmail.co.uk>
2021-10-02 18:38:21 +0200

    firebase docs updated for username change

--- functions/firebase/accounts/updateUserData.ts
+++ functions/firebase/accounts/updateUserData.ts
@@ -1,4 +1,5 @@
-import { firestore } from "../index.js"
+import { firestore } from "../index"
+
 /*
  * Update user document at users/{uid} with passed data.
  * @param  {string} uid
@@ -6,9 +7,28 @@ import { firestore } from "../index.js"
  */
 
 // TODO make generic for all userdata
+const usernameUpdate = async (uid, updateData) =>{
+  var usernameRef = firestore.collection('usernames').where("uid", "==", uid).limit(1)
+  let docData
+  usernameRef.get().then((snap) => {
+      snap.forEach(async (doc) => {
+        docData = doc.data()
+        // saves to new username doc
+        await firestore.collection('usernames').doc(updateData.username).set(docData)
+        // deletes old doc
+        await doc.ref.delete()
+      })
+    })
+}
 
 export async function updateUserData(data, context) {
   const {uid , updateData} = data
   const userRef = firestore.collection('users').doc(uid)
-  return userRef.set({ ...updateData }, { merge: true })
+
+  if (Object.keys(updateData).includes("username")) (await usernameUpdate(uid, updateData))
+
+  userRef.set({ ...updateData }, { merge: true })
+
+  return {status: "success"}
 }
+
