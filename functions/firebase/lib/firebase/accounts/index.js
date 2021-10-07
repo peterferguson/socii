ad2b2a51b485abb2e3f5527ddf1b0857322904de
Peter Ferguson <peterferguson95@gmail.com>
2021-10-07 19:06:30 +0100

    remove tracking of firebase js lib

--- functions/firebase/lib/firebase/accounts/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createAccount = void 0;
-var createAccount_1 = require("./createAccount");
-Object.defineProperty(exports, "createAccount", { enumerable: true, get: function () { return createAccount_1.createAccount; } });
-//# sourceMappingURL=index.js.map
--- functions/firebase/lib/firebase/utils/isSell.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isSell = void 0;
-const isSell = (orderType) => orderType.toLowerCase().includes("sell");
-exports.isSell = isSell;
-//# sourceMappingURL=isSell.js.map
