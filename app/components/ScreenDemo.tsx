eccc95f2248ea59d80827ce86e0a366292fb02b9
Peter Ferguson <peterferguson95@gmail.com>
2021-10-18 13:50:15 +0100

    screens demo working on desktop

--- app/components/ScreenDemo.tsx
+++ app/components/ScreenDemo.tsx
@@ -6,9 +6,50 @@ import Image from "next/image"
 import React, { useEffect, useRef, useState } from "react"
 import { IoIosPeople, IoIosStats } from "react-icons/io"
 import { IoAnalyticsSharp } from "react-icons/io5"
+import { useMediaQuery } from "react-responsive"
 
-// TODO ALIGN INFO UNDER BUTTONS!! (line 97)
-// TODO put line behind image (line 95)
+const screens = [
+  {
+    title: "Search for Stocks",
+    info: "Search for thousands of stocks",
+    Icon: IoAnalyticsSharp,
+    Image: () => (
+      <Image
+        src={"/images/GOOG-iphone-screenshot.png"}
+        alt="socii-GOOG-stock-screenshot"
+        layout="fill"
+        priority={true}
+      />
+    ),
+  },
+  {
+    title: "Share with Groups",
+    info: "Share stock information with your group, direct in your chat",
+    Icon: IoIosPeople,
+    Image: () => (
+      <Image
+        src={"/images/chat-screenshot.png"}
+        alt="socii-chat-screenshot"
+        layout="fill"
+        priority={true}
+      />
+    ),
+  },
+  {
+    title: "Check Other Groups",
+    info: "Compare against other groups and win prizes",
+    Icon: IoIosStats,
+    Image: () => (
+      <Image
+        src={"/images/leaderboard-screenshot.png"}
+        alt="socii-leaderboard-screenshot"
+        layout="fill"
+        priority={true}
+      />
+    ),
+  },
+]
+
 const ScreenDemo = () => {
   const [animated, setAnimated] = React.useState(false)
   const cardsRef = useRef(null)
@@ -20,6 +61,8 @@ const ScreenDemo = () => {
   const [changeDirection, setChangeDirection] = useState<"right" | "left" | undefined>(
     undefined
   )
+
+  const is1Col = !useMediaQuery({ minWidth: 640 })
 
   useEffect(() => {
     setChangeDirection(
@@ -33,168 +76,117 @@ const ScreenDemo = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedScreenIndex])
 
-  // - Define this here to ensure images are loaded before animation
-  const screens = [
-    {
-      title: "Search for Stocks",
-      info: "Search thousands of stocks for price history, charts, or even current news regarding the company",
-      Icon: IoAnalyticsSharp,
-      Image: () => (
-        <Image
-          src={"/images/GOOG-iphone-screenshot.png"}
-          alt="socii-GOOG-stock-screenshot"
-          layout="fill"
-        />
-      ),
-    },
-    {
-      title: "Share with Groups",
-      info: "Share the information you find with your group, direct in your chat",
-      Icon: IoIosPeople,
-      Image: () => (
-        <Image
-          src={"/images/chat-screenshot.png"}
-          alt="socii-chat-screenshot"
-          layout="fill"
-        />
-      ),
-    },
-    {
-      title: "Check Other Groups",
-      info: "Compare yourself against other groups, and win prizes in competitions",
-      Icon: IoIosStats,
-      Image: () => (
-        <Image
-          src={"/images/leaderboard-screenshot.png"}
-          alt="socii-leaderboard-screenshot"
-          layout="fill"
-        />
-      ),
-    },
-  ]
-
-  const IphoneImage = screens[selectedScreenIndex].Image
-
   return (
     <div className="relative h-full">
       <section className="relative h-full max-h-full p-4">
         <div className="absolute inset-0 p-4 sm:p-6 lg:p-8 bg-gradient-to-tr via-white from-white to-palette-lightest" />
         <div className="relative flex p-4 max-w-7xl sm:p-6 lg:p-8 dark:bg-gray-800">
-          <div className="w-full ">
-            <div
-              className="w-full max-w-sm px-8 mx-auto lg:w-1/2 sm:max-w-xl"
-              ref={cardsRef}
+          <div className="w-full px-8 mx-auto sm:mx-0" ref={cardsRef}>
+            <Transition
+              show={cardsVisible || animated}
+              className="flex flex-col items-center justify-between w-full"
+              enter="transition transform delay-500 duration-700 ease-in"
+              enterFrom="opacity-0 translate-y-24"
+              enterTo="translate-y-0"
+              afterEnter={() => setAnimated(true)}
             >
-              <Transition
-                show={cardsVisible || animated}
-                enter="transition transform delay-500 duration-700 ease-in"
-                enterFrom="opacity-0 translate-y-24"
-                enterTo="translate-y-0"
-                afterEnter={() => setAnimated(true)}
-              >
-                <div className="flex flex-col items-center">
-                  <div
-                    ref={cardsRef}
-                    className="bg-gray-50"
-                    style={{
-                      position: "relative",
-                      borderRadius: "2.5rem",
-                      boxShadow: "0px 0px 60px #fff",
-                    }}
-                  >
-                    <IphoneMock>
-                      {/* 💩 solution to having the first image appear before transition */}
-                      {selectedScreenIndex === 0 && changeDirection === undefined && (
-                        <Image
-                          src={"/images/GOOG-iphone-screenshot.png"}
-                          alt="socii-GOOG-stock-screenshot"
-                          layout="fill"
+              <div className="flex flex-col items-center sm:flex-row sm:w-full">
+                {is1Col && (
+                  <Screens
+                    selectedScreenIndex={selectedScreenIndex}
+                    changeDirection={changeDirection}
+                  />
+                )}
+                <div className="flex flex-row pb-3 mt-12 sm:items-center sm:flex-col">
+                  {screens.map((item, i) => (
+                    <>
+                      {i !== 0 && (
+                        <div
+                          key={`line-${i - 1}`}
+                          className={`z-0 w-20 sm:w-0 h-0 sm:h-16 mt-6 sm:mt-0 border ${
+                            selectedScreenIndex > i - 1
+                              ? "border-brand/80"
+                              : "border-brand/40"
+                          }`}
                         />
                       )}
-                      {[0, 1, 2].map((i) => (
-                        <Transition
-                          key={`transition-image-${i}`}
-                          className="absolute inset-0 z-10 flex w-full h-full"
-                          show={
-                            selectedScreenIndex === i && changeDirection !== undefined
-                          }
-                          unmount={false}
-                          enter="transition ease-in-out duration-300 transform"
-                          enterFrom={
-                            changeDirection === "right"
-                              ? "translate-x-full"
-                              : "-translate-x-full"
-                          }
-                          enterTo="translate-x-0"
-                          leave="transition ease-in-out duration-300 transform"
-                          leaveFrom="translate-x-0"
-                          leaveTo={
-                            changeDirection === "right"
-                              ? "-translate-x-full"
-                              : "translate-x-full"
-                          }
-                        >
-                          <IphoneImage />
-                        </Transition>
-                      ))}
-                    </IphoneMock>
-                  </div>
-                  <div className="flex flex-row pb-3 mt-12">
-                    {screens.map((item, i) => (
-                      <>
-                        {i !== 0 && (
+                      <button key={i} onClick={() => setSelectedScreenIndex(i)}>
+                        <div className="flex items-center justify-center">
                           <div
-                            key={`line-${i - 1}`}
-                            className={`z-0 w-20 sm:w-32 h-0 mt-6 border ${
-                              selectedScreenIndex > i - 1
-                                ? "border-brand/80"
-                                : "border-brand/40"
-                            }`}
-                          />
-                        )}
-                        <button key={i} onClick={() => setSelectedScreenIndex(i)}>
-                          <div className="flex items-center justify-center">
-                            <div
-                              className={tw(
-                                "ring-2 z-10 rounded-full p-2 space-x-6",
-                                selectedScreenIndex == i
-                                  ? "gradient-flow opacity-50 ring-purple-500"
-                                  : "ring-brand"
-                              )}
-                            >
-                              <div className="flex items-center justify-center sm:h-10 sm:w-10">
-                                <item.Icon
-                                  className={tw(
-                                    "w-6 h-6 m-1",
-                                    selectedScreenIndex == i
-                                      ? "text-white"
-                                      : "text-brand"
-                                  )}
-                                  aria-hidden="true"
-                                />
-                              </div>
+                            className={tw(
+                              "ring-2 z-10 rounded-full p-2 space-x-6",
+                              selectedScreenIndex == i
+                                ? "gradient-flow opacity-50 ring-purple-500"
+                                : "ring-brand"
+                            )}
+                          >
+                            <div className="flex items-center justify-center sm:h-10 sm:w-10">
+                              <item.Icon
+                                className={tw(
+                                  "w-6 h-6 m-1",
+                                  selectedScreenIndex == i ? "text-white" : "text-brand"
+                                )}
+                                aria-hidden="true"
+                              />
                             </div>
                           </div>
-                        </button>
-                      </>
-                    ))}
-                  </div>
-                  <Transition
-                    className="flex flex-col items-center text-center font-primary "
-                    enter="transition duration-75 ease-in"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                  >
-                    <div className="pb-3 text-lg">
-                      {screens[selectedScreenIndex].title}
-                    </div>
-                    <div className="text-base font-thin sm:text-sm">
-                      {screens[selectedScreenIndex].info}
-                    </div>
-                  </Transition>
+                        </div>
+                      </button>
+                    </>
+                  ))}
                 </div>
-              </Transition>
-            </div>
+                {/* 💩 solution to having the first text appear before transition */}
+                {selectedScreenIndex === 0 && changeDirection === undefined && (
+                  <div className="w-full mx-0 text-center sm:mx-8 sm:w-1/2 sm:text-left font-primary">
+                    <p className="pb-3 text-lg sm:text-5xl whitespace-nowrap">
+                      {screens[0].title}
+                    </p>
+                    <p className="text-base font-thin sm:pl-2 sm:text-lg sm:whitespace-nowrap">
+                      {screens[0].info}
+                    </p>
+                  </div>
+                )}
+                {[0, 1, 2].map((i) => (
+                  <Transition
+                    key={`transition-image-${i}`}
+                    className="flex flex-col items-center justify-center w-full sm:items-start"
+                    show={selectedScreenIndex === i && changeDirection !== undefined}
+                    appear={true}
+                    enter="transition ease-out duration-500 transform"
+                    enterFrom={
+                      changeDirection === "right"
+                        ? "translate-x-24 opacity-0"
+                        : "-translate-x-24 opacity-0"
+                    }
+                    enterTo="translate-x-0 opacity-100"
+                    leave="transition ease-out duration-500 transform"
+                    leaveFrom="translate-x-0 opacity-100"
+                    leaveTo={
+                      changeDirection === "right"
+                        ? "-translate-x-24 opacity-0"
+                        : "translate-x-24 opacity-0"
+                    }
+                  >
+                    {i === selectedScreenIndex && (
+                      <div className="w-full mx-0 text-center sm:mx-8 sm:w-1/2 sm:text-left font-primary">
+                        <p className="pb-3 text-lg sm:text-5xl whitespace-nowrap">
+                          {screens[i].title}
+                        </p>
+                        <p className="text-base font-thin sm:pl-2 sm:text-lg sm:whitespace-nowrap">
+                          {screens[i].info}
+                        </p>
+                      </div>
+                    )}
+                  </Transition>
+                ))}
+                {!is1Col && (
+                  <Screens
+                    selectedScreenIndex={selectedScreenIndex}
+                    changeDirection={changeDirection}
+                  />
+                )}
+              </div>
+            </Transition>
           </div>
         </div>
       </section>
@@ -210,5 +202,73 @@ const IphoneMock = ({ children }) => (
     {children}
   </div>
 )
+
+const Screens = ({ selectedScreenIndex, changeDirection }) => {
+  return (
+    <div
+      className="bg-gray-50"
+      style={{
+        position: "relative",
+        borderRadius: "2.5rem",
+        boxShadow: "0px 0px 60px #fff",
+      }}
+    >
+      <IphoneMock>
+        {/* 💩 solution to having the first image appear before transition */}
+        {selectedScreenIndex === 0 && changeDirection === undefined && (
+          <Image
+            src={"/images/GOOG-iphone-screenshot.png"}
+            alt="socii-GOOG-stock-screenshot"
+            layout="fill"
+            priority={true}
+          />
+        )}
+        {[0, 1, 2].map((i) => (
+          <Transition
+            key={`transition-image-${i}`}
+            className="absolute inset-0 z-10 flex w-full h-full"
+            show={selectedScreenIndex === i && changeDirection !== undefined}
+            unmount={false}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom={
+              changeDirection === "right" ? "translate-x-full" : "-translate-x-full"
+            }
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo={
+              changeDirection === "right" ? "-translate-x-full" : "translate-x-full"
+            }
+          >
+            {i === 0 && (
+              <Image
+                src={"/images/GOOG-iphone-screenshot.png"}
+                alt="socii-GOOG-stock-screenshot"
+                layout="fill"
+                priority={true}
+              />
+            )}
+            {i === 1 && (
+              <Image
+                src={"/images/chat-screenshot.png"}
+                alt="socii-chat-screenshot"
+                layout="fill"
+                priority={true}
+              />
+            )}
+            {i === 2 && (
+              <Image
+                src={"/images/leaderboard-screenshot.png"}
+                alt="socii-leaderboard-screenshot"
+                layout="fill"
+                priority={true}
+              />
+            )}
+          </Transition>
+        ))}
+      </IphoneMock>
+    </div>
+  )
+}
 
 export default ScreenDemo
