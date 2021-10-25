0b20fa657dc7e59471461b2394aff47a5f6f5726
Peter Ferguson <peterferguson95@gmail.com>
2021-10-25 07:14:50 +0100

    update chat style

--- web/packages/app/hooks/useStreamChatTheme.ts
+++ web/packages/app/hooks/useStreamChatTheme.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useColorScheme } from "react-native"
 import type { DeepPartial, Theme } from "stream-chat-expo"
+import tw from "../lib/tailwind"
 
 export const useStreamChatTheme = () => {
   const colorScheme = useColorScheme()
@@ -11,15 +12,16 @@ export const useStreamChatTheme = () => {
         width: 32,
       },
     },
-    colors:
-      colorScheme === "dark"
+    colors: {
+      accent_blue: tw.color("brand"),
+      accent_green: "#20E070",
+      accent_red: "#FF3742",
+      transparent: "transparent",
+      black: tw`text-brand-black dark:text-brand-gray`.color as string,
+      ...(colorScheme === "dark"
         ? {
-            accent_blue: "#005FFF",
-            accent_green: "#20E070",
-            accent_red: "#FF3742",
             bg_gradient_end: "#101214",
             bg_gradient_start: "#070A0D",
-            black: "#FFFFFF",
             blue_alice: "#00193D",
             border: "#141924",
             grey: "#7A7A7A",
@@ -31,18 +33,13 @@ export const useStreamChatTheme = () => {
             overlay_dark: "#FFFFFFCC", // CC = 80% opacity
             shadow_icon: "#00000080", // 80 = 50% opacity
             targetedMessageBackground: "#302D22",
-            transparent: "transparent",
             white: "#101418",
             white_smoke: "#13151B",
             white_snow: "#070A0D",
           }
         : {
-            accent_blue: "#005FFF",
-            accent_green: "#20E070",
-            accent_red: "#FF3742",
             bg_gradient_end: "#F7F7F7",
             bg_gradient_start: "#FCFCFC",
-            black: "#000000",
             blue_alice: "#E9F2FF",
             border: "#00000014", // 14 = 8% opacity; top: x=0, y=-1; bottom: x=0, y=1
             grey: "#7A7A7A",
@@ -54,11 +51,11 @@ export const useStreamChatTheme = () => {
             overlay_dark: "#00000099", // 99 = 60% opacity
             shadow_icon: "#00000040", // 40 = 25% opacity; x=0, y=0, radius=4
             targetedMessageBackground: "#FBF4DD", // dark mode = #302D22
-            transparent: "transparent",
             white: "#FFFFFF",
             white_smoke: "#F2F2F2",
             white_snow: "#FCFCFC",
-          },
+          }),
+    },
     imageGallery: {
       blurType: colorScheme === "dark" ? "dark" : "light",
     },
@@ -69,9 +66,7 @@ export const useStreamChatTheme = () => {
   })
   const [chatStyle, setChatStyle] = useState(getChatStyle())
 
-  useEffect(() => {
-    setChatStyle(getChatStyle())
-  }, [colorScheme])
+  useEffect(() => setChatStyle(getChatStyle()), [colorScheme])
 
   return chatStyle
 }
