24046e197d409f6d72028cee88b1b36e1df73c40
Peter Ferguson <peterferguson95@gmail.com>
2021-10-27 13:30:49 +0100

    link trade confirmation attachment to data & functions

--- web/packages/app/components/CustomAttachments/InvestmentConfirmationAttachment.tsx
+++ web/packages/app/components/CustomAttachments/InvestmentConfirmationAttachment.tsx
@@ -1,6 +1,6 @@
-import LogoPriceCardHeader from "../LogoPriceCardHeader"
+import { useState, useEffect } from "react"
 import { useAuth } from "../../hooks/useAuth"
-// import { agreesToTrade } from "../../lib/firebase/client/db/agreesToTrade"
+import { getTickerISIN, setAgreesToTrade } from "../../lib/firebase/client/db"
 import MMLButton from "./MML/Button"
 import {
   // useChannelStateContext,
@@ -12,39 +12,40 @@ import tw from "../../lib/tailwind"
 import { useStream } from "../../hooks/useStream"
 import { View } from "react-native"
 import AttachmentCardWithLogo from "./AttachmentCardWithLogo"
+import { useTickerPrice } from "../../hooks/useTickerPrice"
 
 const InvestmentConfirmationAttachment = ({ attachment }) => {
   const { user } = useAuth()
   const { channel } = useStream()
-  const { client } = useChatContext()
   const { message } = useMessageContext()
 
-  // const groupName = channel.cid.split(":").pop()
-  // const alpacaAccountId = user.alpacaAccountId
+  const groupName = channel.cid.split(":").pop()
+  const alpacaAccountId = user.alpacaAccountId
 
-  const [action, shares] = message.text.split("to ").pop().split("shares")[0].split(" ")
-  const cost = message.text.split("for ").pop().split(". Do")[0]
+  const { price } = useTickerPrice(attachment?.symbol)
 
-  const onSubmit = async (agrees: boolean) => {}
-  //   // ! Trade is based on the groups selection process.
-  //   // ! Defaults to uanimous decision.
-  //   await toast.promise(
-  //     agreesToTrade(groupName, message.id, user.uid, alpacaAccountId),
-  //     {
-  //       loading: agrees ? "agreeing..." : "disagreeing...",
-  //       success: agrees ? "Agreed to trade" : "Disagreed to trade",
-  //       error: agrees ? "Error agreeing to trade" : "Error disagreeing to trade",
-  //     }
-  //   )
-  //   // await client.partialUpdateMessage(message.id, {
-  //   //   set: { status: "cancelled" },
-  //   // })
-  // }
+  const onSubmit = async (agrees: boolean) => {
+    // ! Trade is based on the groups selection process.
+    // ! Defaults to uanimous decision.
+    await setAgreesToTrade(groupName, message.id, user.uid, alpacaAccountId)
+    //   await toast.promise(
+    //     agreesToTrade(groupName, message.id, user.uid, alpacaAccountId),
+    //     {
+    //       loading: agrees ? "agreeing..." : "disagreeing...",
+    //       success: agrees ? "Agreed to trade" : "Disagreed to trade",
+    //       error: agrees ? "Error agreeing to trade" : "Error disagreeing to trade",
+    //     }
+    //   )
+  }
 
   // TODO: Add different views of the buy card for users who did not submit it
 
   return (
-    <AttachmentCardWithLogo assetSymbol={attachment?.tickerSymbol} isin={""}>
+    <AttachmentCardWithLogo
+      assetSymbol={attachment?.symbol}
+      isin={attachment?.isin}
+      currentPriceData={price}
+    >
       <View style={tw`flex-row items-center justify-center mt-4`}>
         <MMLButton
           key={`no-button`}
