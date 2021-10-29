fac03a436d20dba46ef1ec23ec092dee13eca3a0
James McComish <james.mcc94@hotmail.co.uk>
2021-10-29 23:43:44 +0200

    Main settings page

--- web/packages/app/components/MainSettingsComponent.tsx
+++ web/packages/app/components/MainSettingsComponent.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
+import { Z_FULL_FLUSH } from 'zlib';
+import tw from '../lib/tailwind';
 // import AppModal from '../common/AppModal';
 // import Icon from '../common/Icon';
 
-const SettingsComponent = ({
+const MainSettingsComponent = ({
   modalVisible,
   setModalVisible,
   settingsOptions,
 }) => {
   return (
-    <>
+    <View style={tw.style(`h-11/12 bg-brand-gray-lightest dark:bg-brand-black rounded-tr-lg rounded-tl-lg`)}>
       {/* <AppModal
         modalVisible={modalVisible}
         modalFooter={<></>}
@@ -38,18 +40,19 @@ const SettingsComponent = ({
         title="Sort by"
         setModalVisible={setModalVisible}
       /> */}
-      <ScrollView style={{backgroundColor: "#eeeeee"}}>
+      <ScrollView >
         {settingsOptions.map(({title, subTitle, onPress}, index) => (
           <TouchableOpacity key={title} onPress={onPress}>
             <View
               style={{
-                paddingHorizontal: 20,
-                paddingBottom: 20,
-                paddingTop: 20,
+                paddingHorizontal: 20, 
+                paddingBottom: 10,
+                paddingTop: 10,
+                
               }}>
-              <Text style={{fontSize: 17}}>{title}</Text>
+              <Text style={{fontSize: 17}}>{title} --> </Text>
               {subTitle && (
-                <Text style={{fontSize: 14, opacity: 0.5, paddingTop: 5}}>
+                <Text style={{fontSize: 12, opacity: 0.5, paddingTop: 3}}>
                   {subTitle}
                 </Text>
               )}
@@ -59,8 +62,8 @@ const SettingsComponent = ({
           </TouchableOpacity>
         ))}
       </ScrollView>
-    </>
+    </View>
   );
 };
 
-export default SettingsComponent;
+export default MainSettingsComponent;
--- web/packages/app/screens/settings/settings.tsx
+++ web/packages/app/screens/settings/settings.tsx
@@ -1,9 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {useEffect, useState} from 'react';
-import SettingsComponent from '../../components/SettingsComponent';
+import {View, Text, Image} from 'react-native';
+import MainSettingsComponent from '../../components/MainSettingsComponent';
+import { useAuth } from '../../hooks/useAuth';
+import tw from '../../lib/tailwind';
+
 
 const SettingsScreen = () => {
   const [email, setEmail] = useState(null);
+  const { user, signout } = useAuth()
   const [modalVisible, setModalVisible] = React.useState(false);
 
   const settingsOptions = [
@@ -16,11 +21,32 @@ const SettingsScreen = () => {
   ];
 
   return (
-    <SettingsComponent
-      modalVisible={modalVisible}
-      setModalVisible={setModalVisible}
-      settingsOptions={settingsOptions}
-    />
+    <View style={tw`flex-1 flex-col items-center justify-between bg-blue-600`}>
+      
+        <View style={tw`flex-col items-center pt-10`}>
+          {user?.photoUrl ? (
+              <Image
+                source={user?.photoUrl ? { uri: user.photoUrl } : null}
+                style={{ width: 100, height: 100, borderRadius: 50 }}
+              />
+            ) : (
+              <Text>TMP No Image</Text>
+            )}
+
+          <Text style={tw`text-white text-lg`}>
+            {user?.username}
+          </Text>
+        </View>
+
+        <View  style={tw`flex flex-col items-center justify-end w-full`}>
+          <MainSettingsComponent
+            modalVisible={modalVisible}
+            setModalVisible={setModalVisible}
+            settingsOptions={settingsOptions}
+          /> 
+        </View> 
+      
+    </View>
   );
 };
 
