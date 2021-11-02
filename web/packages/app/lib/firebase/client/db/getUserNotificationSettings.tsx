c119c82bf1a7d04c52fa4e50b6d4bbdaf19bc126
James McComish <james.mcc94@hotmail.co.uk>
2021-11-02 20:02:33 +0100

    notifications settings linked to fs

---
+++ web/packages/app/lib/firebase/client/db/getUserNotificationSettings.tsx
@@ -0,0 +1,13 @@
+import { doc, getDoc } from "firebase/firestore"
+import { db } from "../../."
+
+/*
+ * Gets a users notification settings from users/{uid}/:notifications
+ * @param  {string} uid
+ */
+
+export const getUserNotificationSettings = async (uid: string) => {
+  const userRef = doc(db, `users/${uid}`)
+  const snapshot = await getDoc(userRef)
+  return snapshot.data()?.notifications
+}
