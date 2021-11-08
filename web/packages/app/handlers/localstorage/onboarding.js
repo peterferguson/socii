dfef23715df3c93d5bc6f0f3924190fc95d0e7b1
Peter Ferguson <peterferguson95@gmail.com>
2021-11-08 07:40:30 +0000

    add localstorage onbaording fns

---
+++ web/packages/app/handlers/localstorage/onboarding.js
@@ -0,0 +1,11 @@
+import { getLocal, saveLocal } from "./common"
+
+const onboardingVersion = "0.1.0"
+
+const ONBOARDING_COMPLETED = "onboardingCompleted"
+
+export const getOnboardingComplete = () =>
+  getLocal(ONBOARDING_COMPLETED, onboardingVersion)
+
+export const saveOnboardingComplete = onboardingComplete =>
+  saveLocal(ONBOARDING_COMPLETED, onboardingComplete, onboardingVersion)
