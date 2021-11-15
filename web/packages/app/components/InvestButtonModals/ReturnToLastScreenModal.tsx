563dab8aaba99d2e6209038cd9d8738ba76f9f16
James McComish <james.mcc94@hotmail.co.uk>
2021-11-15 15:21:41 +0100

    added symbols to return to screen

--- web/packages/app/components/InvestButtonModals/ReturnToLastScreenModal.tsx
+++ web/packages/app/components/InvestButtonModals/ReturnToLastScreenModal.tsx
@@ -1,21 +1,40 @@
 import React from "react"
 import { View, Text, Pressable } from "react-native"
 import tw from "app/lib/tailwind"
+import { ArrowRotateRight, DocumentForward } from "iconsax-react-native"
 
 const ReturnToLastScreenModal = ({ send }) => (
-  <View style={tw`inline-block w-full overflow-y-scroll align-middle`}>
+  <View style={tw`inline-block w-full align-middle h-full`}>
     <View style={tw`flex items-center justify-center `}>
       <Pressable
-        style={tw`w-1/2 max-w-lg p-4 my-2 overflow-y-scroll text-left align-middle bg-white shadow-md  transform rounded-2xl`}
+        style={tw`w-4/5 p-4 my-2 bg-white shadow-md transform rounded-2xl`}
         onPress={() => send("DISAGREE")}
       >
-        <Text>No, start over!</Text>
+        <View style={tw`flex flex-row`}>
+          <View style={tw`flex justify-center p-2 rounded-full bg-pink-200`}>
+            <ArrowRotateRight size={25} variant="Outline"/>
+          </View>  
+          <View  style={tw`flex flex-col justify-center pl-2 w-11/12`} >                
+            <Text style={tw`flex flex-wrap text-lg leading-none text-brand-black`}>
+              No, start over!
+            </Text>
+          </View>
+        </View> 
       </Pressable>
       <Pressable
-        style={tw`inline-block w-4/5 max-w-lg p-4 my-2 overflow-y-scroll text-left align-middle bg-white shadow-md  transform rounded-2xl`}
+        style={tw`inline-block w-4/5 p-4 my-2 bg-white shadow-md transform rounded-2xl`}
         onPress={() => send("AGREE")}
       >
-        <Text>Yes, continue</Text>
+        <View style={tw`flex flex-row`}>
+          <View style={tw`flex justify-center p-2 rounded-full bg-brand-lightTeal`}>
+            <DocumentForward size={25} variant="Outline"/>
+          </View>  
+          <View  style={tw`flex flex-col justify-center pl-2 w-11/12`} >                
+            <Text style={tw`flex flex-wrap text-lg leading-none text-brand-black`}>
+              Yes, continue
+            </Text>
+          </View>
+        </View> 
       </Pressable>
     </View>
   </View>
