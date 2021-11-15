424c8461130591d8e57cdd56066e6eb52e1bd19c
James McComish <james.mcc94@hotmail.co.uk>
2021-11-15 14:39:32 +0100

    added symbols to invest action

--- web/packages/app/components/InvestButtonModals/SelectInvestActionModal.tsx
+++ web/packages/app/components/InvestButtonModals/SelectInvestActionModal.tsx
@@ -1,23 +1,14 @@
-import { useAuth } from "app/hooks/useAuth"
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { View, Text, Pressable } from "react-native"
 import { Actions } from "./Actions"
 import tw from "app/lib/tailwind"
 
 const SelectInvestActionModal = ({ symbol, state, send }) => {
-  console.log("in select actionnnnnnnnn")
 
   const setSelectedAction = (action: { name: string; actionName: string }) => {
-    // setActionSelected(`Share ${symbol} with a group`)
-    // send("CHOOSE_SHARE")
     send(action.actionName)
   }
 
-  // const setSelectedGroup = (group) => {
-  //   console.log(("sendinggroup"));
-  //   setGroupSelected(group.name)
-  //   send("SELECT_GROUP", { groupName: group.name })
-  // }
   const actions=Actions(symbol).filter((k) =>
     !state.context.hasHolding ? k.name.match(/Buy|Share/) : true)
 
@@ -27,13 +18,22 @@ const SelectInvestActionModal = ({ symbol, state, send }) => {
         {/** TODO Add a loader here  */}
         {actions?.map(action => (
           <Pressable
-            style={tw`inline-block w-4/5 max-w-lg p-2 my-2 overflow-y-scroll text-left align-middle bg-white shadow-md  transform rounded-2xl`}
+            style={tw`inline-block w-10/12 max-w-lg p-2 my-2 overflow-y-scroll text-left align-middle bg-white shadow-md  transform rounded-2xl`}
             onPress={()=>{setSelectedAction(action)}}
           >
-            <Text style={tw`text-xl leading-none text-brand-black`}>{action.name}</Text>
-            <Text style={tw`text-sm leading-none text-gray-500`}>
-              {action.description}
-            </Text>
+            <View style={tw`flex flex-row`}>
+              <View style={tw`flex justify-center`}>
+                <action.icon/>
+              </View>  
+              <View  style={tw`flex flex-col justify-center pl-2`} >                
+                <Text style={tw`text-xl leading-none text-brand-black`}>
+                  {action.name}
+                </Text>
+                <Text style={tw`text-xs text-left leading-none text-gray-500 pt-1`}>
+                  {action.description}
+                </Text>  
+              </View>
+            </View>           
           </Pressable>
         ))}
       </View>
