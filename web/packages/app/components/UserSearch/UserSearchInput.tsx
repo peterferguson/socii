1b0a36134a7b1c1baaac4b2130e628176ec5c258
Peter Ferguson <peterferguson95@gmail.com>
2021-11-21 22:06:44 +0000

    fix search icon bug

--- web/packages/app/components/UserSearch/UserSearchInput.tsx
+++ web/packages/app/components/UserSearch/UserSearchInput.tsx
@@ -10,10 +10,12 @@ const UserSearchInput = ({ onSubmit, ...props }) => {
   const { clearText, searchText, onFocusInput, onChangeSearchText } = useSearchUsers()
 
   const { onFocus = null, onBlur = null } = props
+  React.useEffect(() => console.log("onFocus", onFocus), [onFocus])
+  React.useEffect(() => console.log("isFocused", isFocused), [isFocused])
 
   return (
     <CenteredRow style={tw`w-11/12 px-1 py-3`}>
-      {(!isFocused || searchText === "") && (
+      {!isFocused && searchText === "" && (
         <UserSearchIcon
           size="24"
           color={tw.color("brand-black/20")}
