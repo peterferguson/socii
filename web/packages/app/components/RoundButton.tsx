19e8ad61cf2cf11614e0260d0e2af34f5f88c4d9
Peter Ferguson <peterferguson95@gmail.com>
2021-11-23 15:22:24 +0000

    add gradient to RoundButton

--- web/packages/app/components/RoundButton.tsx
+++ web/packages/app/components/RoundButton.tsx
@@ -2,16 +2,25 @@ import tw from "app/lib/tailwind"
 import { ArrowRight2 } from "iconsax-react-native"
 import React from "react"
 import { Pressable, Text, View } from "react-native"
+import { LinearGradient } from "expo-linear-gradient"
 
 // TODO: Generalise this to be a reusable component
 export const RoundButton = ({ onPress, label }) => (
-  <Pressable
-    style={tw`justify-center w-full px-4 py-2 bg-teal-200 border border-transparent rounded-full`}
-    onPress={onPress}
-  >
-    <Text style={tw`text-lg text-center font-poppins-500 uppercase text-teal-700`}>{label}</Text>
-    <View style={tw`absolute right-4`}>
-      <ArrowRight2 size="20" color={tw`text-teal-700`.color as string} />
-    </View>
-  </Pressable>
+  <View style={tw`rounded-full overflow-hidden`}>
+    <LinearGradient
+      colors={[tw.color("teal-200"), tw.color("teal-100")]}
+      style={tw.style(`justify-center w-full px-4 py-2`)}
+    >
+      <Pressable style={tw`justify-center w-full px-4 py-2`} onPress={onPress}>
+        <Text
+          style={tw`text-lg text-center font-poppins-500 uppercase text-teal-700 border border-transparent`}
+        >
+          {label}
+        </Text>
+        <View style={tw`absolute -right-1`}>
+          <ArrowRight2 size="20" color={tw`text-teal-700`.color as string} />
+        </View>
+      </Pressable>
+    </LinearGradient>
+  </View>
 )
