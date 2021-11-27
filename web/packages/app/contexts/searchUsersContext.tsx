e0ed73999a5f96c2139772fec0dfa13a8abf143c
james_mccomish <james.mcc94@hotmail.co.uk>
2021-11-27 13:35:55 +0000

    deleted old files

--- web/packages/app/contexts/searchUsersContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react"
-import { PaginatedUsers } from "../hooks/usePaginatedUsers"
-
-export type SearchUsersContext = PaginatedUsers
-export const SearchUsersContext = createContext({} as SearchUsersContext)
