0dfe3023dc45cab2da742fdc35a9339e2e70723e
james_mccomish <james.mcc94@hotmail.co.uk>
2021-11-27 14:04:02 +0000

    corrected text capital letters

---
+++ web/packages/app/components/Text/HeaderText.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Text, TextStyle } from "react-native"
+import tw from "app/lib/tailwind"
+
+const HeaderText = ({ text, style }: { text: string; style?: TextStyle }) => (
+  <Text style={{ ...tw`text-brand-black font-poppins-400 text-2xl`, ...style }}>
+    {text}
+  </Text>
+)
+
+export default HeaderText
---
+++ web/packages/app/components/Text/index.ts
@@ -0,0 +1,2 @@
+export { default as HeaderText } from "./HeaderText"
+export { default as TruncatedText } from "./TruncatedText"
